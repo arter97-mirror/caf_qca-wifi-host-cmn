5b4846499e72e47788d8b6ea7d6abe2b6cfe396e
Mithun Kikkeri Bhaskarashetty <mkikkeri@qti.qualcomm.com>
2023-06-06 14:42:30 +0800

    qcacmn: Add roam control attrs for roam params change dynamically
    Add roam control attrs for the feature of Changing Roaming Rarameters
    Dynamically.
    
    Change-Id: Ib150db4b2954bafd199ae9d8c66eef795c608fc0
    CRs-Fixed: 4472367

--- os_if/linux/qca_vendor.h
+++ os_if/linux/qca_vendor.h
@@ -4487,6 +4487,20 @@ enum qca_vendor_attr_roam_candidate_selection_criteria {
  *	interval until a candidate is found.
  *	This configuration is valid until next the next disconnection. If this
  *	attribute is not present, the existing configuration shall be used.
+ *
+ * @QCA_ATTR_ROAM_CONTROL_NEIGHBOR_LOOKUP_RSSI_THRESHOLD: Signed 8-bit value
+ *	in dBm.
+ *	This attribute is used to control the RSSI threshold for neighbor
+ *	lookup. It is used to set value of neighbor lookup rssi to wlan driver
+ *	and wlan firmware, and it is also used to get the value of neighbor
+ *	lookup rssi from wlan driver.
+ *
+ * @QCA_ATTR_ROAM_CONTROL_ROAM_RSSI_DIFF: Unsigned 8-bit value.
+ *	This attribute is used to decide whether to Roam or not based on RSSI.
+ *	AP1 is the currently associated AP and AP2 is chosen for roaming. The
+ *	Roaming will happen only if AP2 has better Signal Quality and it has a
+ *	RSSI better than AP2. RoamRssiDiff is the number of units AP2 is better
+ *	than AP1.
  */
 enum qca_vendor_attr_roam_control {
 	QCA_ATTR_ROAM_CONTROL_ENABLE = 1,
@@ -4510,6 +4524,8 @@ enum qca_vendor_attr_roam_control {
 	QCA_ATTR_ROAM_CONTROL_HO_DELAY_FOR_RX = 25,
 	QCA_ATTR_ROAM_CONTROL_FULL_SCAN_NO_REUSE_PARTIAL_SCAN_FREQ = 26,
 	QCA_ATTR_ROAM_CONTROL_FULL_SCAN_6GHZ_ONLY_ON_PRIOR_DISCOVERY = 27,
+	QCA_ATTR_ROAM_CONTROL_NEIGHBOR_LOOKUP_RSSI_THRESHOLD = 28,
+	QCA_ATTR_ROAM_CONTROL_ROAM_RSSI_DIFF = 29,
 	QCA_ATTR_ROAM_CONTROL_CONNECTED_HIGH_RSSI_OFFSET = 31,
 	QCA_ATTR_ROAM_CONTROL_CANDIDATE_SCORE_WEIGHTAGE_2P4GHZ = 32,
 	QCA_ATTR_ROAM_CONTROL_CANDIDATE_SCORE_WEIGHTAGE_5GHZ = 33,
