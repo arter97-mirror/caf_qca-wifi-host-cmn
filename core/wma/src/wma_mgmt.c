sonyxperiadev/vendor-qcom-opensource-wlan-qcacld-3.0

qcacld-3.0: Rate limit excessive logging
Rate limit excessive logging to prevent kernel panic.

Change-Id: I916fd576c4fd20ebe4f49e546cc5180bd0a79b1a
CRs-Fixed: 3348753

core/wma/src/wma_mgmt.c
@@ -3475,7 +3475,7 @@ int wma_process_rmf_frame(tp_wma_handle wma_handle,
 				return -EINVAL;
 			}
 		} else {
-			wma_err("Rx unprotected unicast mgmt frame");
+			wma_err_rl("Rx unprotected unicast mgmt frame");
 			rx_pkt->pkt_meta.dpuFeedback =
 				DPU_FEEDBACK_UNPROTECTED_ERROR;
 		}

arter97-mirror/caf_qca-wifi-host-cmn

core/wma/src/wma_mgmt.c
@@ -3164,7 +3164,7 @@ int wma_process_rmf_frame(tp_wma_handle wma_handle,
 				return -EINVAL;
 			}
 		} else {
-			wma_err("Rx unprotected unicast mgmt frame");
+			wma_debug_rl("Rx unprotected unicast mgmt frame");
 			rx_pkt->pkt_meta.dpuFeedback =
 				DPU_FEEDBACK_UNPROTECTED_ERROR;
 		}