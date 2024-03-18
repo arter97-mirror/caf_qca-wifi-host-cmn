4f827795a959919bd772a4af83951cf2031b3ade
Qun Zhang <quic_qunz@quicinc.com>
2024-03-18 10:16:32 +0800

    qcacmn: Support LFR2 wpa2/wpa3 mix roaming
    To support LFR2 wpa2/wpa3 mix roaming for cert case
    SAE-5.8.3, add below changes for LFR2 roaming:
    1. with existing STA code changes, The vdev crypto params
    are filled in cm_connect_active() but when STA tries to roam
    from WPA2 AP to WPA3 AP or vice versa, the new keymgmt of AP2
    (AP2 keymgmt is FT-SAE if STA roams from WPA2 to WPA3 and AP2
    keymgmt is FT-PSK if STA roams from WPA3 to WPA2 AP's) is not
    updating in the vdev crypto params instead it holds the AP1
    keymgmt. So remove MACRO CONN_MGR_ADV_FEATURE for function
    cm_fill_roam_vdev_crypto_params.
    2. when there has multiple key mgmt, choosing one keymgmt and
    one ucastcipherset based on higher security for host roaming
    case is needed as well. So add similar function
    cm_update_per_peer_crypto_params_for_roam for LFR2 case.
    
    Change-Id: Ie023c4cd9ba5f2a1cf9d89bd7fb5ade681e92728
    CRs-Fixed: 3705702
    (cherry picked from commit 89b75ec030949b9d243eb4b0ae87decdd813be95)

--- umac/mlme/connection_mgr/core/src/wlan_cm_connect.c
+++ umac/mlme/connection_mgr/core/src/wlan_cm_connect.c
@@ -1891,7 +1891,6 @@ void cm_update_ml_partner_info(struct wlan_cm_connect_req *req,
 }
 #endif
 
-static
 void cm_update_per_peer_key_mgmt_crypto_params(struct wlan_objmgr_vdev *vdev,
 					struct security_info *neg_sec_info)
 {
@@ -1968,7 +1967,6 @@ void cm_update_per_peer_key_mgmt_crypto_params(struct wlan_objmgr_vdev *vdev,
 	neg_sec_info->key_mgmt = key_mgmt;
 }
 
-static
 void cm_update_per_peer_ucastcipher_crypto_params(struct wlan_objmgr_vdev *vdev,
 					struct security_info *neg_sec_info)
 {
