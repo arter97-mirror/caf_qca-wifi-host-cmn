2b92bb41997bc8d16bcbc2bad1f8ca44a3fae345
Yu Ouyang <quic_yuo@quicinc.com>
2024-05-13 14:04:00 +0800

    qcacmn: Increase asysn_task scheduler priority
    For auto standalone HL SPs, increase async_task scheduler priority
    to improve KPI.
    
    Change-Id: I6cf39ae49c9ede85545031ef4f055a26631908d5

--- hif/src/sdio/transfer/mailbox.c
+++ hif/src/sdio/transfer/mailbox.c
@@ -44,6 +44,7 @@
 #include "if_sdio.h"
 #include "regtable.h"
 #include "transfer.h"
+#include <uapi/linux/sched/types.h>
 
 /*
  * The following commit was introduced in v5.17:
@@ -1932,6 +1933,7 @@ QDF_STATUS hif_disable_func(struct hif_sdio_dev *device,
 QDF_STATUS hif_enable_func(struct hif_softc *ol_sc, struct hif_sdio_dev *device,
 			   struct sdio_func *func, bool resume)
 {
+	struct sched_param param = {.sched_priority = 99};
 	QDF_STATUS ret = QDF_STATUS_SUCCESS;
 
 	HIF_ENTER();
@@ -1954,6 +1956,7 @@ QDF_STATUS hif_enable_func(struct hif_softc *ol_sc, struct hif_sdio_dev *device,
 			hif_err("Error creating async task");
 			return QDF_STATUS_E_FAILURE;
 		}
+		sched_setscheduler(device->async_task, SCHED_FIFO, &param);
 		device->is_disabled = false;
 		wake_up_process(device->async_task);
 	}
