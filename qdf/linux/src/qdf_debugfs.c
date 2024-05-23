a8662b6173a0bf9d3259aa34a8882712162e0363
Guisen Yang <quic_guiseny@quicinc.com>
2024-05-23 10:14:48 +0800

    qcacmn: Use the name of THIS_MODULE as the debugfs folder name
    The value of KBUILD_MODNAME varies in different kernel, to ensure
    the consistency, the name of THIS_MODULE is more appropriate to
    be the folder name of debugfs.
    
    Change-Id: Idc18f5e28f84568ea973b4590cf136aab8f5d9e2
    CRs-Fixed: 3821460

--- qdf/linux/src/qdf_debugfs.c
+++ qdf/linux/src/qdf_debugfs.c
@@ -37,7 +37,7 @@ static qdf_dentry_t qdf_debugfs_root;
 
 QDF_STATUS qdf_debugfs_init(void)
 {
-	qdf_debugfs_root = debugfs_create_dir(KBUILD_MODNAME, NULL);
+	qdf_debugfs_root = debugfs_create_dir(module_name(THIS_MODULE), NULL);
 
 	if (!qdf_debugfs_root)
 		return QDF_STATUS_E_FAILURE;
