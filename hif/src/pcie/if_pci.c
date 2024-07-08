b964729b2db1dd133d4dcbb61bc9b69c71cf52e0
Guisen Yang <quic_guiseny@quicinc.com>
2024-07-08 15:01:14 +0800

    qcacmn: Do not check pci addr boundary for ROME
    Do not check pci addr boundary for AR6320 when reading one reg.
    
    Change-Id: Ie3d0a5e275f1709c6ab265d6a28d6478c9de2009
    CRs-Fixed: 3863050

--- hif/src/pcie/if_pci.c
+++ hif/src/pcie/if_pci.c
@@ -3954,6 +3954,7 @@ int hif_pci_addr_in_boundary(struct hif_softc *scn, uint32_t offset)
 	    tgt_info->target_type == TARGET_TYPE_QCA6490 ||
 	    tgt_info->target_type == TARGET_TYPE_QCN7605 ||
 	    tgt_info->target_type == TARGET_TYPE_QCA8074 ||
+	    tgt_info->target_type == TARGET_TYPE_AR6320 ||
 	    tgt_info->target_type == TARGET_TYPE_KIWI) {
 		/*
 		 * Need to consider offset's memtype for QCA6290/QCA8074,
