947eaa5e239840a90c59e2b360556b31719d431c
Anuj Khera <quic_akhera@quicinc.com>
2024-07-15 18:35:11 +0530

    qcacld-3.0: Check for WLAN and IPA shared SMMU enabled
    Add support to check if WLAN and IPA shared SMMU
    context bank is supported by platform driver.
    
    Change-Id: I35a14dc5e23aa3f8057ab4eac0161bcf95e96c95
    CRs-Fixed: 3880123

--- core/pld/inc/pld_common.h
+++ core/pld/inc/pld_common.h
@@ -1219,6 +1219,16 @@ int pld_force_wake_request(struct device *dev);
 bool pld_is_direct_link_supported(struct device *dev);
 
 /**
+ * pld_is_ipa_shared_smmu_enable() - Get whether shared ctx bank is supported
+ *				     by IPA or not
+ * @dev: device
+ *
+ * Return: true if supported
+ *         false on failure or if not supported
+ */
+bool pld_is_ipa_shared_smmu_enable(struct device *dev);
+
+/**
  * pld_force_wake_request_sync() - Request to awake MHI synchronously
  * @dev: device
  * @timeout_us: timeout in micro-sec request to wake
