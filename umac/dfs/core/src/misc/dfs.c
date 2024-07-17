a2155a18e7050881a4cb771580299c6aabae2465
Will Huang <quic_wilhuang@quicinc.com>
2024-07-17 11:12:02 +0800

    qcacmn: Enable sub channel marking for DFS radar
    It is partial pick change I374ed76c6a13ef5fac1a78073f1b8974b0a9bb30,
    only enable sub channel marking for DFS channel which is a new
    requirement, but disable puncture which don't support on target chip.
    
    Change-Id: I374ed76c6a13ef5fac1a78073f1b8974b0a9bb30
    CRs-Fixed: 3386020

--- umac/dfs/core/src/misc/dfs.c
+++ umac/dfs/core/src/misc/dfs.c
@@ -183,6 +183,24 @@ int dfs_create_object(struct wlan_dfs **dfs)
 	return 0;
 }
 
+#if defined(CONFIG_REG_CLIENT)
+static void dfs_puncture_init(struct wlan_dfs *dfs)
+{
+	/*
+	 * Enable sub chan DFS type if QCA_DFS_BW_PUNCTURE defined, or all
+	 * bonded operation freq will be affected and disabled for nol,
+	 * puncture can't work, always need to switch freq.
+	 */
+	dfs_set_nol_subchannel_marking(dfs, true);
+
+	/* Don't enable puncture which not support */
+}
+#else
+static inline void dfs_puncture_init(struct wlan_dfs *dfs)
+{
+}
+#endif
+
 int dfs_attach(struct wlan_dfs *dfs)
 {
 	int ret;
@@ -213,6 +231,9 @@ int dfs_attach(struct wlan_dfs *dfs)
 	 * and full offload, indicating test mode timer initialization for both.
 	 */
 	dfs_main_task_testtimer_init(dfs);
+
+	dfs_puncture_init(dfs);
+
 	return 0;
 }
 
