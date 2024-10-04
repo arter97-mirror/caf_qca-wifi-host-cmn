50f9f67ca201b954b214b89d60b581566488c7d3
Rachit Kankane <quic_rkankane@quicinc.com>
2024-10-04 17:42:04 +0530

    qcacmn: Populate rssi while calculating bss score
    As part of previous code refactor, rssi_score variable was leftout
    without any initialization. Which was also getting printed giving
    wrong showing 0 rssi.
    Populate rssi_score value calculated while calculating bss score.
    
    Change-Id: Iadf6f111a1bbf92e158901a9979685c1a3710420
    CRs-Fixed: 3948111

--- umac/mlme/connection_mgr/core/src/wlan_cm_bss_scoring.c
+++ umac/mlme/connection_mgr/core/src/wlan_cm_bss_scoring.c
@@ -2478,6 +2478,7 @@ cm_sort_vendor_algo_mlo_bss_entry(struct wlan_objmgr_psoc *psoc,
  * @bss_mlo_type: Bss MLO type
  * @pcl_chan_weight:  PCL channel weight
  * @rssi_prorated_pct: RSSI prorated pencentage
+ * @rssi_score: Calculated RSSI
  *
  * For MLO AP, consider partner link to calculate combined score,
  * For legacy/SLO AP or link, get total score of RSSI, bandwidth,
@@ -2492,10 +2493,10 @@ static int cm_calculate_ml_scores(struct wlan_objmgr_psoc *psoc,
 				  qdf_list_t *scan_list, uint8_t ml_flag,
 				  enum MLO_TYPE bss_mlo_type,
 				  int pcl_chan_weight,
-				  uint8_t *rssi_prorated_pct)
+				  uint8_t *rssi_prorated_pct,
+				  int32_t *rssi_score)
 {
 	int32_t score = 0;
-	int32_t rssi_score = 0;
 	int32_t congestion_pct = 0;
 	int32_t bandwidth_score = 0;
 	int32_t congestion_score = 0;
@@ -2508,7 +2509,7 @@ static int cm_calculate_ml_scores(struct wlan_objmgr_psoc *psoc,
 	    bss_mlo_type == MLSR || bss_mlo_type == EMLSR ||
 	    bss_mlo_type == MLO_TYPE_MAX ||
 	    !wlan_cm_is_eht_allowed_for_current_security(psoc, entry, false)) {
-		rssi_score =
+		*rssi_score =
 			cm_calculate_rssi_score(&score_config->rssi_score,
 						entry->rssi_raw,
 						weight_config->rssi_weightage);
@@ -2516,7 +2517,7 @@ static int cm_calculate_ml_scores(struct wlan_objmgr_psoc *psoc,
 			cm_get_rssi_prorate_pct(&score_config->rssi_score,
 						entry->rssi_raw,
 						weight_config->rssi_weightage);
-		score += rssi_score;
+		score += *rssi_score;
 		bandwidth_score =
 			cm_get_bw_score(weight_config->chan_width_weightage,
 					cm_get_ch_width(entry, phy_config),
@@ -2761,7 +2762,8 @@ static int cm_calculate_bss_score(struct wlan_objmgr_psoc *psoc,
 					   phy_config, scan_list,
 					   ml_flag, bss_mlo_type,
 					   pcl_chan_weight,
-					   &prorated_pcnt);
+					   &prorated_pcnt,
+					   &rssi_score);
 	score += ml_score;
 
 	/*
