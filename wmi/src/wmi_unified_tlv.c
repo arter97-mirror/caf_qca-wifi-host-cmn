0e8b9186af4592a3a5b5179d06e5e9e023ea21ef
Vinod Kumar Pirla <quic_vpirla@quicinc.com>
2024-10-06 22:58:50 -0700

    qcacmn: APIs to init and deinit zero power scan resources
    APIs to initialize and deinitialize resources for zero power scan
    feature.
    
    Change-Id: I533c130c5e040af0f39fd1873c9d0c0402b5762f
    CRs-Fixed: 3956246

--- wmi/src/wmi_unified_tlv.c
+++ wmi/src/wmi_unified_tlv.c
@@ -24082,6 +24082,10 @@ static void populate_tlv_events_id(WMI_EVT_ID *event_ids)
 #ifdef FEATURE_MGMT_RX_OVER_SRNG
 	event_ids[wmi_mgmt_srng_reap_eventid] = WMI_MGMT_SRNG_REAP_EVENTID;
 #endif
+#ifdef FEATURE_WLAN_ZERO_POWER_SCAN
+	event_ids[wmi_scan_cache_result_eventid] =
+					WMI_SCAN_CACHE_RESULT_EVENTID;
+#endif
 }
 
 #ifdef WLAN_FEATURE_LINK_LAYER_STATS
@@ -24750,6 +24754,10 @@ static void populate_tlv_service(uint32_t *wmi_service)
 	wmi_service[wmi_service_therm_throt_5_levels] =
 				WMI_SERVICE_THERM_THROT_5_LEVELS;
 	wmi_service[wmi_service_mrsno_support] = WMI_SERVICE_MULTI_RSNO_SUPPORT;
+#ifdef FEATURE_WLAN_ZERO_POWER_SCAN
+	wmi_service[wmi_service_scan_cache_report_support] =
+			WMI_SERVICE_SCAN_CACHE_REPORT_SUPPORT;
+#endif
 }
 
 /**
