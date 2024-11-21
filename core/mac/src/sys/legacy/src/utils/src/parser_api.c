935e9fd375d8f24abe5f528600df233dab8209bd
Sheenam Monga <quic_shemonga@quicinc.com>
2024-11-22 01:42:35 +0530

    qcacld-3.0: Set minimum tx power correctly
    currently, if fw sends negative power value
    then 8 dbm will be set as minimum power because
    of typecasting. Negative power value will become
    greater than 8 and finally 8 will be advertised
    as minimum supported power value.
    
    Fix is to convert power to original power before
    comparison, check sign bit set and convert power
    to original power send by halphy before comparison.
    Range that host can support is 127 to -128.
    
    Change-Id: Id1cefd470b580ff92194291e3eb3526753cd1f38
    CRs-Fixed: 3967113

--- core/mac/src/sys/legacy/src/utils/src/parser_api.c
+++ core/mac/src/sys/legacy/src/utils/src/parser_api.c
@@ -76,6 +76,7 @@
 #define WMM_OUI_TYPE_SIZE  5
 
 #define RSN_OUI_SIZE 4
+#define SIGN_BIT 7
 /* ////////////////////////////////////////////////////////////////////// */
 #ifdef WLAN_FEATURE_FILS_SK_SAP
 static void fils_convert_assoc_req_frame2_struct(tDot11fAssocRequest *ar,
@@ -2105,12 +2106,26 @@ populate_dot11f_measurement_report2(struct mac_context *mac,
 } /* End PopulatedDot11fMeasurementReport2. */
 #endif
 
+static
+int8_t populate_decimal_min_power(uint8_t power)
+{
+	int is_power_negative;
+
+	is_power_negative = (power & (1 << SIGN_BIT)) != 0;
+
+	if (is_power_negative)
+		return power | ~((1 << SIGN_BIT) - 1);
+	else
+		return power;
+}
+
 void
 populate_dot11f_power_caps(struct mac_context *mac,
 			   tDot11fIEPowerCaps *pCaps,
 			   uint8_t nAssocType, struct pe_session *pe_session)
 {
 	struct vdev_mlme_obj *mlme_obj;
+	int8_t min_power;
 
 	pCaps->minTxPower = pe_session->min_11h_pwr;
 	pCaps->maxTxPower = pe_session->maxTxPower;
@@ -2120,9 +2135,10 @@ populate_dot11f_power_caps(struct mac_context *mac,
 	if (mlme_obj && mlme_obj->mgmt.generic.tx_pwrlimit)
 		pCaps->maxTxPower = mlme_obj->mgmt.generic.tx_pwrlimit;
 
+	min_power = populate_decimal_min_power(mlme_obj->mgmt.generic.minpower);
+
 	if (mlme_obj && mlme_obj->mgmt.generic.minpower)
-		pCaps->minTxPower = QDF_MIN(pCaps->minTxPower,
-					    mlme_obj->mgmt.generic.minpower);
+		pCaps->minTxPower = QDF_MIN(pCaps->minTxPower, min_power);
 
 	pCaps->present = 1;
 } /* End populate_dot11f_power_caps. */
