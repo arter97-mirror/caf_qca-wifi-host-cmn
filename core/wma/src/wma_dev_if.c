053e6dd56085b8f146fb8ce5c214240e8c9a74dc
Nijun Gong <quic_ngong@quicinc.com>
2024-11-26 08:31:24 +0530

    qcacld-3.0: Avoid phymode and chwidth mismatch
    For chips supporting 11AX, WMI_HOST_PEER_CHWIDTH shall be sent prior
    to WMI_HOST_PEER_PHYMODE. For other chips WMI_HOST_PEER_PHYMODE shall
    be sent firstly. This is to avoid firmware assert caused by phymode
    and chwidth mismatch.
    Also, no need to send phymode during opmode update, with band width
    less or equal to phymode.
    
    Change-Id: I7d2b2f5cf70aec8d432b07ac4eb944476aa59ccb
    CRs-Fixed: 3954644

--- core/wma/src/wma_dev_if.c
+++ core/wma/src/wma_dev_if.c
@@ -927,6 +927,50 @@ static void wma_handle_hidden_ssid_restart(tp_wma_handle wma,
 				      0, NULL);
 }
 
+static void wma_set_phy_mode_n_bw(tp_wma_handle wma, uint8_t vdev_id,
+				  uint8_t *peer_addr, uint32_t phymode,
+				  uint32_t max_ch_width_supported)
+{
+	wma_set_peer_param(wma, peer_addr, WMI_PEER_PHYMODE, phymode, vdev_id);
+	wma_set_peer_param(wma, peer_addr, WMI_HOST_PEER_CHWIDTH,
+			   max_ch_width_supported, vdev_id);
+}
+
+static void wma_set_bw_n_phy_mode(tp_wma_handle wma, uint8_t vdev_id,
+				  uint8_t *peer_addr, uint32_t phymode,
+				  bool is_eht, struct wlan_objmgr_vdev *vdev,
+				  struct wlan_channel *vdev_chan,
+				  uint16_t puncture_bitmap,
+				  uint32_t max_ch_width_supported)
+{
+	uint32_t bw_puncture = 0;
+	uint16_t new_puncture_bitmap = 0;
+	enum phy_ch_width new_bw;
+
+	new_bw =
+	target_if_wmi_chan_width_to_phy_ch_width(max_ch_width_supported);
+
+	if (is_eht) {
+		wlan_reg_extract_puncture_by_bw(vdev_chan->ch_freq,
+						puncture_bitmap,
+						vdev_chan->ch_freq,
+						vdev_chan->ch_freq_seg2,
+						new_bw,
+						&new_puncture_bitmap);
+		QDF_SET_BITS(bw_puncture, 0, 8, new_bw);
+		QDF_SET_BITS(bw_puncture, 8, 16, new_puncture_bitmap);
+		wma_debug("new punct: 0x%x", new_puncture_bitmap);
+		wlan_util_vdev_peer_set_param_send(vdev, peer_addr,
+						   WLAN_MLME_PEER_BW_PUNCTURE,
+						   bw_puncture);
+	} else {
+		wma_set_peer_param(wma, peer_addr, WMI_HOST_PEER_CHWIDTH,
+				   max_ch_width_supported, vdev_id);
+	}
+
+	wma_set_peer_param(wma, peer_addr, WMI_PEER_PHYMODE, phymode, vdev_id);
+}
+
 void
 wma_send_peer_phy_mode(tSirMacAddr bssId, uint8_t vdev_id,
 		       enum wlan_phymode phy_mode)
@@ -1007,9 +1051,6 @@ static void wma_peer_send_phymode(struct wlan_objmgr_vdev *vdev,
 	uint8_t vdev_id;
 	bool is_eht = false;
 	uint16_t puncture_bitmap = 0;
-	uint16_t new_puncture_bitmap = 0;
-	uint32_t bw_puncture = 0;
-	enum phy_ch_width new_bw;
 
 	if (wlan_peer_get_peer_type(peer) == WLAN_PEER_SELF)
 		return;
@@ -1051,32 +1092,19 @@ static void wma_peer_send_phymode(struct wlan_objmgr_vdev *vdev,
 	max_ch_width_supported =
 		wmi_get_ch_width_from_phy_mode(wma->wmi_handle,
 					       fw_phymode);
-	new_bw =
-	target_if_wmi_chan_width_to_phy_ch_width(max_ch_width_supported);
 
-	if (is_eht) {
-		wlan_reg_extract_puncture_by_bw(vdev_chan->ch_width,
-						puncture_bitmap,
-						vdev_chan->ch_freq,
-						vdev_chan->ch_freq_seg2,
-						new_bw,
-						&new_puncture_bitmap);
-		QDF_SET_BITS(bw_puncture, 0, 8, new_bw);
-		QDF_SET_BITS(bw_puncture, 8, 16, new_puncture_bitmap);
-		wlan_util_vdev_peer_set_param_send(vdev, peer_mac_addr,
-						   WLAN_MLME_PEER_BW_PUNCTURE,
-						   bw_puncture);
-	} else {
-		wma_set_peer_param(wma, peer_mac_addr, WMI_HOST_PEER_CHWIDTH,
-				   max_ch_width_supported, vdev_id);
-	}
+	/* For non 11ax capable targets send phymode followed by width */
+	if (!IS_FEATURE_SUPPORTED_BY_FW(DOT11AX))
+		wma_set_phy_mode_n_bw(wma, vdev_id, peer_mac_addr, fw_phymode,
+				      max_ch_width_supported);
+	else
+		wma_set_bw_n_phy_mode(wma, vdev_id, peer_mac_addr, fw_phymode,
+				      is_eht, vdev, vdev_chan, puncture_bitmap,
+				      max_ch_width_supported);
 
-	wma_set_peer_param(wma, peer_mac_addr, WMI_HOST_PEER_PHYMODE,
-			   fw_phymode, vdev_id);
-	wma_debug("FW phymode %d old phymode %d new phymode %d bw %d punct: 0x%x macaddr " QDF_MAC_ADDR_FMT,
+	wma_debug("FW phymode %d old phymode %d new phymode %d bw %d macaddr " QDF_MAC_ADDR_FMT,
 		  fw_phymode, old_peer_phymode, new_phymode,
-		  max_ch_width_supported, new_puncture_bitmap,
-		  QDF_MAC_ADDR_REF(peer_mac_addr));
+		  max_ch_width_supported, QDF_MAC_ADDR_REF(peer_mac_addr));
 }
 
 static
