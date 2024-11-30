54040ee09419a50735320e96fb482b38584172d5
Jyoti Kumari <quic_jyotkuma@quicinc.com>
2024-11-30 18:01:01 +0530

    qcacld-3.0: Don't allow CSA only for p2p_go when eapol in prog
    Currently, driver doesn't allow CSA for both SAP and P2P GO when
    EAPOL is in progress but for SAP it doesn't need to check the
    EAPOL in progress as the sta or p2p_client channel will be
    already decided. So when SAP needs to do SCC with STA channel and
    if STA's eapol is in progress then driver will not allow to
    change the channel and SCC will fail.
    
    To avoid this, check eapol in progress only for p2p_go mode
    
    Change-Id: I4259020ccf228f3be6d7333c2c5a757a36d628f2
    CRs-Fixed: 3988440

--- core/hdd/src/wlan_hdd_hostapd.c
+++ core/hdd/src/wlan_hdd_hostapd.c
@@ -3258,7 +3258,8 @@ static int hdd_softap_unpack_ie(mac_handle_t mac_handle,
 	return QDF_STATUS_SUCCESS;
 }
 
-bool hdd_is_any_sta_connecting(struct hdd_context *hdd_ctx)
+bool hdd_is_any_sta_connecting(struct hdd_context *hdd_ctx,
+			       enum QDF_OPMODE op_mode)
 {
 	struct hdd_adapter *adapter = NULL, *next_adapter = NULL;
 	struct hdd_station_ctx *sta_ctx;
@@ -3281,7 +3282,17 @@ bool hdd_is_any_sta_connecting(struct hdd_context *hdd_ctx)
 					sme_is_sta_key_exchange_in_progress(
 							hdd_ctx->mac_handle,
 							adapter->vdev_id);
-			if (is_connecting || key_exchng_in_prog) {
+
+			/* In case of P2P GO + STA/CLI concurrency, when EAPOL
+			 * is in progress for STA/CLI, the P2P GO will still be
+			 * in NOA and if CSA is allowed then CSA frames won't
+			 * go over the air as P2P GO is in NOA.
+			 * In case of SAP, it doesn't need to check the EAPOL
+			 * in progress as the STA/CLI channel will be already
+			 * decided after getting connect complete indication.
+			 */
+			if ((op_mode == QDF_P2P_GO_MODE &&
+			     key_exchng_in_prog) || is_connecting) {
 				hdd_debug("vdev_id %d: connecting %d key_exchng_in_prog %d",
 					  adapter->vdev_id, is_connecting,
 					  key_exchng_in_prog);
@@ -3339,7 +3350,7 @@ int hdd_softap_set_channel_change(struct net_device *dev, int target_chan_freq,
 	 * cannot do CSA as it won't be able to send CSA frames during NOA
 	 * period
 	 */
-	if (hdd_is_any_sta_connecting(hdd_ctx) ||
+	if (hdd_is_any_sta_connecting(hdd_ctx, adapter->device_mode) ||
 	    (adapter->device_mode == QDF_P2P_GO_MODE &&
 	     ucfg_p2p_is_p2p_go_noa_in_progress(hdd_ctx->pdev,
 						adapter->vdev_id))) {
