db75d93aaf5b7d255f4c278b2bd80b7236dd122c
Abhinav Kumar <quic_abhikuma@quicinc.com>
2024-12-31 01:31:40 -0800

    qcacld-3.0: Update NUD tracking behavior
    Set INI "gEnableNUDTracking" to 0 by default.
    This way, the driver does not track NUD failures
    and ignores them.
    
    Change-Id: Ib84c39a1ac1520bbdfa9fe75fbd9d2167598dcf8
    CRs-Fixed: 4013836

--- components/dp/dispatcher/inc/wlan_dp_cfg.h
+++ components/dp/dispatcher/inc/wlan_dp_cfg.h
@@ -1107,7 +1107,7 @@
  * gEnableNUDTracking - Will enable or disable NUD tracking within driver
  * @Min: 0
  * @Max: 3
- * @Default: 2
+ * @Default: 0
  *
  * This ini is used to specify the behaviour of the driver for NUD tracking.
  * If the ini value is:-
@@ -1129,7 +1129,7 @@
 		CFG_INI_UINT("gEnableNUDTracking", \
 		 0, \
 		 3, \
-		 2, \
+		 0, \
 		 CFG_VALUE_OR_DEFAULT, "Driver NUD tracking behaviour")
 
 #define CFG_DP_ENABLE_NUD_TRACKING_ALL \
