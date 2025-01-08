1f61ce9d27c258ba4bd79582887c9e6d880d239b
Vinod Kumar Pirla <quic_vpirla@quicinc.com>
2025-01-08 03:40:09 -0800

    qcacmn: Fill MLO connect resp params even if BSS not found
    Driver fills kernel data structure to notify the MLO connection
    for each link ID and prepares a bitmap of all the valid links.
    If valid links is zero, then kernel expects valid value in
    zeroth index of the connected links data to find the BSSID,
    if valid links is non-zero then kernel expects valid value in
    ap mld address field to get the MLD address of the connection.
    
    Driver fills the connect params only if the kernel returns valid
    BSS pointer in cfg80211_get_bss() API call, if for some reason
    kernel doesn't have the BSS entry for the AP to which driver
    connected then driver doesn't fill the valid links bitmap also
    doesn't fill the BSSID at zeroth index. This fails kernel checks
    to have valid BSSID at zeroth index if valid links bitmap is zero.
    
    Even if cfg80211_get_bss() returns NULL, fill valid links bitmap
    and fill the BSSID from the driver internal data structures to
    avoid sending invalid values to kernel.
    
    Change-Id: I5e904e46f366433f2efbec66ad879ce4dafad953
    CRs-Fixed: 3986358

--- os_if/linux/mlme/src/osif_cm_connect_rsp.c
+++ os_if/linux/mlme/src/osif_cm_connect_rsp.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2012-2015, 2020-2021 The Linux Foundation. All rights reserved.
- * Copyright (c) 2021-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2021-2025 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -535,17 +535,17 @@ static
 void osif_populate_connect_response_for_link(struct wlan_objmgr_vdev *vdev,
 					     struct cfg80211_connect_resp_params *conn_rsp_params,
 					     uint8_t link_id,
-					     uint8_t *link_addr,
+					     uint8_t *link_addr, uint8_t *bssid,
 					     enum wlan_status_code link_status_code,
 					     struct cfg80211_bss *bss)
 {
-	if (bss) {
-		conn_rsp_params->valid_links |=  BIT(link_id);
-		conn_rsp_params->links[link_id].bssid = bss->bssid;
+	conn_rsp_params->valid_links |=  BIT(link_id);
+	conn_rsp_params->links[link_id].bssid = bssid;
+	conn_rsp_params->links[link_id].addr = link_addr;
+	if (bss)
 		conn_rsp_params->links[link_id].bss = bss;
-		conn_rsp_params->links[link_id].addr = link_addr;
-		osif_populate_link_status_code(conn_rsp_params, link_id, link_status_code);
-	}
+
+	osif_populate_link_status_code(conn_rsp_params, link_id, link_status_code);
 
 	mlo_mgr_osif_update_connect_info(vdev, link_id);
 }
@@ -606,6 +606,7 @@ osif_populate_partner_links_mlo_params(struct wlan_objmgr_vdev *vdev,
 		osif_populate_connect_response_for_link(vdev, conn_rsp_params,
 							link_id,
 							link_vdev->vdev_mlme.macaddr,
+							rsp_partner_info->link_addr.bytes,
 							rsp_partner_info->link_status_code,
 							bss);
 
@@ -638,6 +639,7 @@ static void osif_fill_connect_resp_mlo_params(struct wlan_objmgr_vdev *vdev,
 	osif_populate_connect_response_for_link(vdev, conn_rsp_params,
 						assoc_link_id,
 						vdev->vdev_mlme.macaddr,
+						rsp->bssid.bytes,
 						rsp->status_code,
 						bss);
 
@@ -673,6 +675,7 @@ osif_populate_partner_links_mlo_params(struct wlan_objmgr_vdev *vdev,
 		osif_populate_connect_response_for_link(vdev, conn_rsp_params,
 							link_id,
 							link_info->link_addr.bytes,
+							link_info->ap_link_addr.bytes,
 							link_info->link_status_code,
 							bss);
 	}
@@ -709,6 +712,7 @@ static void osif_fill_connect_resp_mlo_params(struct wlan_objmgr_vdev *vdev,
 	osif_populate_connect_response_for_link(vdev, conn_rsp_params,
 						assoc_link_id,
 						link_info->link_addr.bytes,
+						rsp->bssid.bytes,
 						rsp->status_code,
 						bss);
 	osif_populate_partner_links_mlo_params(vdev, rsp, conn_rsp_params);
@@ -829,7 +833,7 @@ void osif_populate_connect_response_for_link(
 			struct wlan_objmgr_vdev *vdev,
 			struct cfg80211_connect_resp_params *conn_rsp_params,
 			uint8_t link_id,
-			uint8_t *link_addr,
+			uint8_t *link_addr, uint8_t *bssid,
 			enum wlan_status_code link_status_code,
 			struct cfg80211_bss *bss)
 {
@@ -918,6 +922,7 @@ osif_populate_partner_links_mlo_params(
 		osif_populate_connect_response_for_link(
 				vdev, conn_rsp_params, link_id,
 				link_vdev->vdev_mlme.macaddr,
+				rsp_partner_info->link_addr.bytes,
 				rsp_partner_info->link_status_code,
 				bss);
 
@@ -967,6 +972,7 @@ static void osif_fill_connect_resp_mlo_params(
 	osif_populate_connect_response_for_link(vdev, conn_rsp_params,
 						assoc_link_id,
 						vdev->vdev_mlme.macaddr,
+						rsp->bssid.bytes,
 						rsp->status_code,
 						bss);
 	osif_populate_partner_links_mlo_params(vdev, rsp, conn_rsp_params);
