a65f9e60ac5b63046967679ce9716dead53e1d28
Asutosh Mohapatra <quic_asutmoha@quicinc.com>
2025-01-16 01:07:54 -0800

    qcacmn: Send both EIRP and PSD TPC if AP advertises both
    Currently, the host sends both PSD and EIRP TPC to the FW
    if the connection power type is SP and the FW has advertised
    the capability to process both PSD and EIRP. However, there
    is a possibility that these two conditions might be true, but
    the AP might have advertised only one of the EIRP or PSD TPE
    IE. In such cases, the host will send both EIRP and PSD TPC to
    the FW, but one will be empty. Since the FW expects both to be
    filled, it will reject the command, and the TPC won't get set.
    
    To address this issue, add a check to send both EIRP and PSD
    TPC only if the AP has advertised both.
    
    Change-Id: I60d7cb2e2df0de2ae80e09fb50ab5a392be34957
    CRs-Fixed: 4039501

--- wmi/src/wmi_unified_tlv.c
+++ wmi/src/wmi_unified_tlv.c
@@ -22328,7 +22328,8 @@ bool is_both_psd_eirp_support_present_for_sp(wmi_unified_t wmi_handle,
 	return (is_service_enabled_tlv(wmi_handle,
 		    WMI_SERVICE_BOTH_PSD_EIRP_FOR_AP_SP_CLIENT_SP_SUPPORT) &&
 		(param->power_type_6g == REG_STANDARD_POWER_AP ||
-			param->is_power_type_client_sp));
+			param->is_power_type_client_sp) &&
+		param->num_psd_pwr_levels && param->num_eirp_pwr_levels);
 }
 
 #if defined(CONFIG_REG_CLIENT) && defined(CONFIG_BAND_6GHZ)
