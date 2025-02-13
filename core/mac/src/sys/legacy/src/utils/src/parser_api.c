18de4760fe6029b1e6b7961d1324d59b08d5f525
Aasir Rasheed <quic_arasheed@quicinc.com>
2025-02-13 10:03:23 +0530

    qcacld-3.0: Fill First Element of tDot11fIEtransmit_power_env
    Currently, the host driver fills all elements of the
    tDot11fIEtransmit_power_env IE inside the tDot11fIEChannelSwitchWrapper
    IE. However, due to auto parsing, the host driver does not fill the TPE
    for 320 MHz, which was introduced in 802.11be_D4.1.
    
    This change ensures that only the first IE of
    tDot11fIEtransmit_power_env is filled to avoid any discrepancies with
    320 MHz.
    
    Change-Id: I2b6bf0d2ad359063bd3688e05aecb9c5dcfbe774
    CRs-Fixed: 4054795

--- core/mac/src/sys/legacy/src/utils/src/parser_api.c
+++ core/mac/src/sys/legacy/src/utils/src/parser_api.c
@@ -770,7 +770,7 @@ populate_dot11f_chan_switch_wrapper(struct mac_context *mac,
 				pe_session->gLimChannelSwitch.ch_width,
 				pe_session->gLimChannelSwitch.sw_target_freq,
 				&num_tpe, true);
-		pDot11f->num_transmit_power_env = num_tpe;
+		pDot11f->num_transmit_power_env = 1;
 	}
 }
 
