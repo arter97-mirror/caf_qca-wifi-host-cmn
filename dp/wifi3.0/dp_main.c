430c020dff49100076dcdd41ce19eec04b6febbf
AJAY SINGH DAHIYA <asinghda@qti.qualcomm.com>
2025-04-07 17:35:22 +0530

    qcacmn: Added HAPS feature to avoid unwanted FW wakeups
    Host Aware Power Save (HAPS) is a feature that, when enables,
    allows the firmware to update the host on its power states.
    The host then checks these states before deciding whether to
    coalesce packets. This process helps prevent unnecessary wakeups
    and also starts qtimer synchronization if enabled.
    
    Change-Id: I75e1e504c7afd5af36cb233f3b9574c886a07175
    CRs-Fixed: 4185584

--- dp/wifi3.0/dp_main.c
+++ dp/wifi3.0/dp_main.c
@@ -12700,6 +12700,12 @@ static struct cdp_fse_ops dp_ops_fse = {
 };
 #endif
 
+#ifdef WLAN_HAPS_ENABLE
+static struct cdp_haps_ops dp_ops_haps = {
+	.haps_handle_ind = dp_haps_handle_ind,
+};
+#endif
+
 #ifdef CONFIG_SAWF_DEF_QUEUES
 static struct cdp_sawf_ops dp_ops_sawf = {
 	.sawf_def_queues_map_req = dp_sawf_def_queues_map_req,
@@ -13611,6 +13617,9 @@ static void dp_soc_txrx_ops_attach(struct dp_soc *soc)
 #endif
 #ifdef WLAN_SUPPORT_RX_FLOW_TAG
 	soc->cdp_soc.ops->fse_ops = &dp_ops_fse;
+#endif
+#ifdef WLAN_HAPS_ENABLE
+	soc->cdp_soc.ops->haps_ops = &dp_ops_haps;
 #endif
 };
 
