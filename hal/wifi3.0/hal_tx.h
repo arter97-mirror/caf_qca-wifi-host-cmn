cd8a27055bb383294e346e9f67b77475e063f2e1
Jinwei Chen <quic_jinweic@quicinc.com>
2025-04-17 01:30:50 -0700

    qcacmn: Copy TX complete info properly for fig
    The format of transmission info from TX Comp ring descriptor
    has been updated on fig compared to peach, modify the format
    of "struct hal_tx_completion_status" to adapt this change.
    
    Change-Id: I4ff442c01cecc2b4a09d84ff08747263a71afcd1
    CRs-Fixed: 4121016

--- hal/wifi3.0/hal_tx.h
+++ hal/wifi3.0/hal_tx.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2016-2021 The Linux Foundation. All rights reserved.
- * Copyright (c) 2021-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2021-2025 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -239,7 +239,9 @@ enum hal_tx_comp_rel_src {
  *       <enum 2     1_6_us_sgi > HE related GI
  *       <enum 3     3_2_us_sgi > HE
  * @mcs: Transmit MCS Rate
+ * @unequal_modulation: unequal modulation information for transmission
  * @ofdma: Set when the transmission was an OFDMA transmission
+ * @reserved_bn: reserved
  * @tones_in_ru: The number of tones in the RU used.
  * @reserved5: reserved
  * @tsf: Lower 32 bits of the TSF
@@ -269,14 +271,22 @@ struct hal_tx_completion_status {
 		 stbc:1,
 		 ldpc:1,
 		 sgi:2,
+#ifdef CONFIG_BORON
+		 mcs:5,
+		 unequal_modulation:3,
+		 ofdma:1,
+		 reserved_bn:4,
+		 tones_in_ru:4,
+#else
 		 mcs:4,
 		 ofdma:1,
 		 tones_in_ru:12,
-	#ifdef TX_NSS_STATS_SUPPORT
+#endif
+#ifdef TX_NSS_STATS_SUPPORT
 		 tx_nss:3;
-	#else
+#else
 		 reserved5:3;
-	#endif
+#endif
 	uint32_t tsf;
 	uint32_t peer_id:16,
 		 tid:8,
