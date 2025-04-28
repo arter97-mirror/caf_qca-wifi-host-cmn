72f6c2d7ee5818952370183d2008e793a0f4f7d4
Arsalan Sariya <asariya@qti.qualcomm.com>
2025-04-28 18:36:21 +0530

    qcacmn: Enable history records for datapath CE
    Enable history records for copy engine owned by datapath.
    The records are enabled for specific events only.
    
    Change-Id: I5f7a31359e441151032c43601a0e65ec2c1557be
    CRs-Fixed: 4229535

--- hif/inc/hif.h
+++ hif/inc/hif.h
@@ -792,7 +792,8 @@ hif_display_ctrl_traffic_pipes_state(struct hif_opaque_softc *hif_ctx)
 }
 #endif
 
-#if defined(HIF_CONFIG_SLUB_DEBUG_ON) || defined(HIF_CE_DEBUG_DATA_BUF)
+#if defined(HIF_CONFIG_SLUB_DEBUG_ON) || defined(HIF_CE_DEBUG_DATA_BUF) ||\
+	defined(RECORD_DP_CE_EVTS)
 void hif_display_latest_desc_hist(struct hif_opaque_softc *hif_ctx);
 #else
 static
