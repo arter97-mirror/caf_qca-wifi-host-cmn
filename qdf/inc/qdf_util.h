fc702605d092d3855f8174be7f268fabd1f8e80c
Divyesh Kapadiya <dkapadiy@qti.qualcomm.com>
2025-05-09 07:21:37 -0700

    qcacmn: Added qdf API for raw_smp_processor_id
    Kernel continuously throws warnings in logcat due to preemption
    being enabled and the use of the smp_processor_id API.
    
    Adding support for raw_smp_processor_id, which retrieves the
    CPU ID even when preemption is enabled, without causing warnings.
    
    Change-Id: I547a9f3dec38507507e7ff6add2925f4bf8f58d9
    CRs-Fixed: 4145061
    (cherry picked from commit e98af4b054174f36c7a33397b056d92abfc6989a)

--- qdf/inc/qdf_util.h
+++ qdf/inc/qdf_util.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2014-2021 The Linux Foundation. All rights reserved.
- * Copyright (c) 2021-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -985,6 +985,19 @@ static inline int qdf_ffs64(uint64_t x)
 static inline int qdf_get_smp_processor_id(void)
 {
 	return __qdf_get_smp_processor_id();
+}
+
+/**
+ * qdf_get_raw_smp_processor_id() - Get the current CPU id
+ *
+ * This API should be called with pre-emption disabled,
+ * otherwise the value will be stale at use.
+ *
+ * Return: current CPU id
+ */
+static inline int qdf_get_raw_smp_processor_id(void)
+{
+	return __qdf_get_raw_smp_processor_id();
 }
 
 /**
