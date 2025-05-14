c541e6458de20228f9db245c8eff65728b803d5f
Neetendra Singh <neesing@qti.qualcomm.com>
2025-05-14 17:44:49 +0530

    qcacmn: Populate wmi services for nan dfs channels
    Integrate WMI service to manage NAN DFS channels from firmware.
    
    CRs-Fixed: 4161827
    Change-Id: Iedd4733a7471536967305f67371bf036c3750ff8

--- wmi/src/wmi_unified_tlv.c
+++ wmi/src/wmi_unified_tlv.c
@@ -25388,7 +25388,8 @@ static void populate_tlv_service(uint32_t *wmi_service)
 				WMI_SERVICE_PER_VDEV_TWT_RESP_DISABLE_SUPPORT;
 	wmi_service[wmi_service_vendor_oui_action_v2] =
 				WMI_SERVICE_VENDOR_OUI_ACTION_V2;
-
+	wmi_service[wmi_service_ndp_dfs_channel_support] =
+				WMI_SERVICE_NDP_DFS_CHANNEL_SUPPORT;
 }
 
 /**
