790b4c735199d674deeef32d3417f58cff42aa65
Dolly Kumari <dollkuma@qti.qualcomm.com>
2025-05-20 06:08:13 -0700

    qcacmn: Initialize filter delete status
    This change resolves the use of an uninitialized
    variable by explicitly initializing the filter
    delete status in QIPS.
    
    CRs-Fixed: 4154454
    Change-Id: Ie2f4cfc1125e476c1ab3730f453ba43ab979f940

--- ipa/core/src/wlan_ipa_core.c
+++ ipa/core/src/wlan_ipa_core.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2013-2021 The Linux Foundation. All rights reserved.
- * Copyright (c) 2021-2025 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -7734,7 +7734,7 @@ int wlan_ipa_wdi_opt_dpath_ctrl_flt_rem_cb(
 	uint8_t num_flts;
 	uint32_t i, j;
 	void *htc_handle;
-	QDF_STATUS status;
+	QDF_STATUS status = QDF_STATUS_SUCCESS;
 	bool delete_all = false;
 	bool valid = false;
 	int code;
