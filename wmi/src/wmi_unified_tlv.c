7c2c42fe486788239c0ac50d0824358077cfd117
Sheenam Monga <shemonga@qti.qualcomm.com>
2025-06-05 13:09:26 +0530

    qcacmn: Add support to check FW cap
    Add support to check if FW is able to handle
    both PSD and EIRP powers together or not.
    
    Change-Id: I49884608e967aea5793862550bc2f72fe884602d
    CRs-Fixed: 3936590

--- wmi/src/wmi_unified_tlv.c
+++ wmi/src/wmi_unified_tlv.c
@@ -21966,7 +21966,6 @@ send_both_eirp_psd_for_set_tpc_tlv(wmi_unified_t wmi_handle,
 	return ret;
 }
 
-static inline
 bool is_both_psd_eirp_support_present_for_sp(wmi_unified_t wmi_handle,
 					     struct reg_tpc_power_info *param)
 {
