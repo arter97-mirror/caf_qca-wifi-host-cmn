e349cf35ea6d9ac8e7fa3cbdf666687a6fe4579b
gaolez <gaolez@qti.qualcomm.com>
2025-07-09 19:54:22 +0800

    qcacmn: Fix compile issue when not enabling ENH CFR
    The extract_cfr_phase_param of the wmi_ops structure is
    only defined when WLAN_ENH_CFR_ENABLE is enabled.
    However, references to this member were not properly
    guarded, leading to compilation errors when the macro
    is disabled.
    
    This change adds appropriate WLAN_ENH_CFR_ENABLE guards
    to the references of extract_cfr_phase_param.
    
    Change-Id: I036b7c19a8274dcca6ec34704d4bc17f37191ce4
    CRs-Fixed: 4270989

--- wmi/src/wmi_unified_cfr_tlv.c
+++ wmi/src/wmi_unified_cfr_tlv.c
@@ -514,6 +514,7 @@ static inline void wmi_enh_cfr_attach_tlv(wmi_unified_t wmi_handle)
 	ops->send_cfr_rcc_cmd = send_cfr_rcc_cmd_tlv;
 	ops->extract_cfr_capture_filter_resp =
 		extract_cfr_capture_filter_event_tlv;
+	ops->extract_cfr_phase_param = extract_cfr_phase_param_tlv;
 }
 #else
 static inline void wmi_enh_cfr_attach_tlv(wmi_unified_t wmi_handle)
@@ -541,7 +542,6 @@ void wmi_cfr_attach_tlv(wmi_unified_t wmi_handle)
 	ops->send_peer_cfr_capture_cmd = send_peer_cfr_capture_cmd_tlv;
 	ops->extract_cfr_peer_tx_event_param =
 		extract_cfr_peer_tx_event_param_tlv;
-	ops->extract_cfr_phase_param = extract_cfr_phase_param_tlv;
 	wmi_cfr_attach_enh_aoa_tlv(ops);
 	wmi_enh_cfr_attach_tlv(wmi_handle);
 }
