69efddd95b0f278bb6186d82349ab09087c20e15
Jianmin Zhu <jianminz@qti.qualcomm.com>
2025-07-16 07:01:49 -0700

    qcacmn: Prevent out-of-bounds access when extracting roam stats
    Fix an issue where roam_scan_trigger_count in the fixed_param of roam
    stats may exceed the actual number of roam_result entries, potentially
    leading to an out-of-bounds access.
    
    To address this, add a check to validate that roam_scan_trigger_count does
    not exceed the number of available roam_result entries. If it does, return
    a failure to prevent invalid memory access.
    
    Change-Id: I4ad68e4e753472e537a5b0850e434fef62bbe637
    CRs-Fixed: 4217001

--- wmi/src/wmi_unified_tlv.c
+++ wmi/src/wmi_unified_tlv.c
@@ -21264,11 +21264,16 @@ extract_roam_trigger_stats_tlv(wmi_unified_t wmi_handle, void *evt_buf,
 		return QDF_STATUS_E_FAILURE;
 	}
 
-	if (!param_buf->roam_result || idx >= param_buf->num_roam_result)
+	if (!param_buf->roam_result || idx >= param_buf->num_roam_result) {
 		wmi_err("roam_result or idx error.%u", idx);
+		return QDF_STATUS_E_FAILURE;
+	}
 
-	if (!param_buf->roam_scan_info || idx >= param_buf->num_roam_scan_info)
+	if (!param_buf->roam_scan_info ||
+	    idx >= param_buf->num_roam_scan_info) {
 		wmi_err("roam_scan_info or idx error.%u", idx);
+		return QDF_STATUS_E_FAILURE;
+	}
 
 	trig->present = true;
 
