914d0ac37754abd3755e42dc73e3e91e0e320a7b
Arti Sahu <artisahu@qti.qualcomm.com>
2025-08-19 05:04:59 -0700

    qcacmn: Fix rx_bufs_reaped bounds and tx_status_info addr2 cast
    This change addresses two issues:
    
    1. Prevent array out-of-bounds access for rx_bufs_reaped:
       The rx_bufs_reaped array has a size of MAX_PDEV_CNT, but previously
       there was no check to ensure lmac_id is within valid bounds before
       accessing the array. This could result in memory corruption.
      Add a check to ensure lmac_id is within [0, MAX_PDEV_CNT).
    
    2. Correct typecast for writing 2 bytes to tx_status_info->addr2[4]:
       Previously, a uint32_t* typecast was used to write at
       &tx_status_info->addr2[4], which writes 4 bytes and can overwrite
       unintended memory. Change the typecast to uint16_t* to ensure only
       2 bytes (addr2[4] and addr2[5]) are updated.
    These changes improve code safety and prevent memory corruption.
    
    CRs-Fixed: 4260126
    Change-Id: I5c0addf3e1c51a011e5d49bdc6062742c6e2779f

--- dp/wifi3.0/dp_rx_err.c
+++ dp/wifi3.0/dp_rx_err.c
@@ -2172,7 +2172,7 @@ more_data:
 			int lmac_id;
 
 			lmac_id = dp_rx_err_exception(soc, ring_desc);
-			if (lmac_id >= 0)
+			if (lmac_id >= 0 && lmac_id < MAX_PDEV_CNT)
 				rx_bufs_reaped[lmac_id] += 1;
 			goto next_entry;
 		}
