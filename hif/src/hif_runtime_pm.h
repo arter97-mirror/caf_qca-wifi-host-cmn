9f7e431153c1a21525bca886a9d6e68b123cc12b
Asutosh Mohapatra <asutmoha@qti.qualcomm.com>
2025-08-25 07:49:43 -0700

    qcacmn: Set HIF_RTPM_DELAY_MIN to 44 ms for TBTT retry
    Set HIF_RTPM_DELAY_MIN to 44 ms to enable RTPM suspend retry after
    a TBTT-related nack from firmware.
    
    CRs-Fixed: 4496736
    Change-Id: I7c0543f1e656ca7d5159a2cd7acfc50c9081ba9b

--- hif/src/hif_runtime_pm.h
+++ hif/src/hif_runtime_pm.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2013-2021 The Linux Foundation. All rights reserved.
- * Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -162,7 +162,7 @@ struct hif_rtpm_ctx {
 	struct hif_rtpm_last_busy_hist *busy_hist[CE_COUNT_MAX];
 };
 
-#define HIF_RTPM_DELAY_MIN 100
+#define HIF_RTPM_DELAY_MIN 44
 #define HIF_RTPM_DELAY_MAX 10000
 
 /**
