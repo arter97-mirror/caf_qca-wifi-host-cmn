b31bb76ffba166f938710240f325ae688a687eb4
Amit Mehta <amitmeht@qti.qualcomm.com>
2025-09-19 17:35:37 +0530

    qcacmn: initialize frame_info to avoid garbage values
    In dp_rx_mon_parse_desc_buffer(), the frame_info structure was not
    initialized, which could lead to uninitialized members containing
    garbage values.
    
    Fix this by explicitly initializing frame_info to zero.
    
    CRs-Fixed: 4297253
    Change-Id: Id21ebdaab595c7d9bef9acfe2a4430caa7597d91

--- dp/wifi3.0/monitor/1.0/dp_rx_mon_1.0.h
+++ dp/wifi3.0/monitor/1.0/dp_rx_mon_1.0.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2021-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -812,7 +812,7 @@ dp_rx_mon_parse_desc_buffer(struct dp_soc *dp_soc,
 			    void **first_rx_desc_tlv,
 			    bool *is_frag_non_raw_p, void *data)
 {
-	struct hal_rx_mon_dest_buf_info frame_info;
+	struct hal_rx_mon_dest_buf_info frame_info = {0};
 	uint32_t rx_pkt_tlv_len = dp_rx_mon_get_rx_pkt_tlv_size(dp_soc);
 
 	/*
