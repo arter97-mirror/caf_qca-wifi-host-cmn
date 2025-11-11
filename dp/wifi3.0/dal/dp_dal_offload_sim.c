697c500a8924635e82c7c54ac1a63815caa76834
Arsalan Sariya <asariya@qti.qualcomm.com>
2025-11-11 10:55:26 +0530

    qcacmn: Add base files for offload simulation
    Add base files for dal offload simulation.
    
    Change-Id: I8a0085636eac003aae87a22babe2044dfcf47352
    CRs-Fixed: 4350774

---
+++ dp/wifi3.0/dal/dp_dal_offload_sim.c
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: ISC
+ */
+
+ #ifdef FEATURE_DAL_DP_SUPPORT
+
+QDF_STATUS offload_sim_init(struct dp_dal_sim_ctx *dal_sim_ctx)
+{
+	/*TODO Implementation*/
+	return QDF_STATUS_SUCCESS;
+}
+
+ #endif /* FEATURE_DAL_DP_SUPPORT */
---
+++ dp/wifi3.0/dal/dp_dal_offload_sim.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: ISC
+ */
+
+#ifndef DP_DAL_OFFLOAD_SIM_H
+#define DP_DAL_OFFLOAD_SIM_H
+
+#ifdef FEATURE_DAL_DP_SUPPORT
+/**
+ * enum offload_sim_ring_type - Ring type for interrupt handling
+ * @OFFLOAD_SIM_RING_TYPE_RX: RX ring (REO destination)
+ * @OFFLOAD_SIM_RING_TYPE_TX_CPL: TX completion ring (WBM2SW)
+ */
+enum offload_sim_ring_type {
+	OFFLOAD_SIM_RING_TYPE_RX = 0,
+	OFFLOAD_SIM_RING_TYPE_TX_CPL = 1,
+};
+
+/**
+ * offload_sim_init() - Initialize offload simulation context
+ * @dal_sim_ctx: Pointer to DAL simulation context
+ *
+ * This function allocates and initializes the offload simulation context.
+ * It assigns values from dal_sim_srng structures (present in dal_sim_ctx)
+ * to dal_vndr_hal_srng structures in the offload_sim_ctx.
+ *
+ * Return: QDF_STATUS_SUCCESS on success, error code on failure
+ */
+QDF_STATUS offload_sim_init(struct dp_dal_sim_ctx *dal_sim_ctx);
+
+#endif /* FEATURE_DAL_DP_SUPPORT */
+#endif /* DP_DAL_OFFLOAD_SIM_H */
