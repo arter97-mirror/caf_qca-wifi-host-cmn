c56b2483157a069bcb3244d99e2dd8b0c3b1e266
Arsalan Sariya <asariya@qti.qualcomm.com>
2025-11-16 16:43:02 +0530

    qcacmn: Add offload sim init/deinit changes
    In init sequence allocate the offload sim
    context and save the ring info passed from
    dal sim. In deinit sequence free the offload sim context.
    
    Change-Id: Ie5449ba4d4995843a5711e1c2a4c6955d0e14a37
    CRs-Fixed: 4354793

--- dp/wifi3.0/dal/dp_dal_offload_sim.c
+++ dp/wifi3.0/dal/dp_dal_offload_sim.c
@@ -11,14 +11,131 @@
 #define OFFLOAD_SIM_IRQ_NAME_LEN 40
 #ifdef FEATURE_DP_DAL_SIM
 
+/**
+ * dp_dal_offload_sim_hal_ring_init() - Init dal_vndr_hal_srng structure.
+ * @offload_sim_ctx: offload sim ctx
+ * @hal_srng: Pointer to destination dal_vndr_hal_srng structure
+ * @sim_srng: Pointer to source dal_sim_srng structure
+ *
+ * Copies ring information field by field from dal_sim_srng to dal_vndr_hal_srng
+ */
+static void dp_dal_offload_sim_hal_ring_init(
+	struct dp_dal_offload_sim_ctx *offload_sim_ctx,
+	struct dal_vndr_hal_srng *hal_srng,
+	struct dal_sim_srng *sim_srng)
+{
+	/* Copy basic ring information */
+}
 int dp_dal_offload_sim_init(struct dp_dal_sim_ctx *dal_sim_ctx)
 {
-	/*TODO Implementation*/
+	struct dp_dal_offload_sim_ctx *offload_ctx;
+	int i;
+	int status = 0;
+
+	if (!dal_sim_ctx) {
+		dp_err("NULL DAL sim context");
+		return -EINVAL;
+	}
+
+	/* Allocate offload simulation context */
+	offload_ctx = qdf_mem_malloc(sizeof(*offload_ctx));
+	if (!offload_ctx) {
+		dp_err("Failed to allocate offload sim context");
+		return -ENOMEM;
+	}
+	/* Zero out the structure */
+	qdf_mem_zero(offload_ctx, sizeof(*offload_ctx));
+
+	offload_ctx->hal_soc.ops = qdf_mem_malloc(
+					sizeof(*offload_ctx->hal_soc.ops));
+	if (!offload_ctx->hal_soc.ops) {
+		status = -ENOMEM;
+		goto free_offload_ctx;
+	}
+
+	/* Store the dev base addr here from dal_sim_ctx.
+	 * Google DAL has to assign BAR address here.
+	 */
+	offload_ctx->hal_soc.dev_base_addr = dal_sim_ctx->dev_base_addr;
+	/* Copy dal_vndr_hal_srng from dal_sim_srng structures */
+	/* RX rings */
+	for (i = 0; i < DAL_RX_RINGS_MAX; i++) {
+		dp_dal_offload_sim_hal_ring_init(
+					offload_ctx,
+					&offload_ctx->rx_ring_hal_srng[i],
+					&dal_sim_ctx->rx_ring[i]);
+	}
+
+	/* TX completion rings */
+	for (i = 0; i < DAL_TX_RINGS_MAX; i++) {
+		dp_dal_offload_sim_hal_ring_init(
+					offload_ctx,
+					&offload_ctx->tx_cmpl_ring_hal_srng[i],
+					&dal_sim_ctx->tx_cmpl_ring[i]);
+	}
+
+	/* TX rings */
+	for (i = 0; i < DAL_TX_RINGS_MAX; i++) {
+		dp_dal_offload_sim_hal_ring_init(
+					offload_ctx,
+					&offload_ctx->tx_ring_hal_srng[i],
+					&dal_sim_ctx->tx_ring[i]);
+	}
+
+	/* RX refill ring */
+	dp_dal_offload_sim_hal_ring_init(offload_ctx,
+					 &offload_ctx->rx_refill_ring_hal_srng,
+					 &dal_sim_ctx->rx_refill_ring);
+
+	/* Vendor HAL ops can be overridden here if needed with target_type*/
+	dal_vndr_hal_default_ops_attach_be(&offload_ctx->hal_soc);
+
+	/* Mark as offload_sim_ctx_initialized */
+	offload_ctx->offload_sim_ctx_initialized = true;
+
+	/* Assign to dal_sim_ctx */
+	dal_sim_ctx->offload_sim_ctx = offload_ctx;
+
 	return 0;
+free_offload_ctx:
+	qdf_mem_free(offload_ctx);
+	return status;
 }
 
 void dp_dal_offload_sim_deinit(struct dp_dal_sim_ctx *dal_sim_ctx)
 {
+	struct dp_dal_offload_sim_ctx *offload_ctx;
+
+	if (!dal_sim_ctx) {
+		dp_warn("NULL DAL sim context in deinit");
+		return;
+	}
+
+	offload_ctx =
+		(struct dp_dal_offload_sim_ctx *)dal_sim_ctx->offload_sim_ctx;
+	if (!offload_ctx) {
+		dp_warn("NULL offload sim context in deinit");
+		return;
+	}
+
+	dp_info("Deinitializing offload simulation context");
+
+	/* Free IRQs before deinitializing */
+	dp_dal_offload_sim_free_irq(dal_sim_ctx);
+
+	/* Mark as not initialized */
+	offload_ctx->offload_sim_ctx_initialized = false;
+	/* Free hal_soc ops memory allocated in init */
+	qdf_mem_free(offload_ctx->hal_soc.ops);
+	/* Free the context */
+	qdf_mem_free(offload_ctx);
+
+	/* Nullify the pointer in dal_sim_ctx */
+	dal_sim_ctx->offload_sim_ctx = NULL;
 	dp_info("Offload simulation context deinitialized successfully");
+}
+
+void dp_dal_offload_sim_free_irq(struct dp_dal_sim_ctx *dal_sim_ctx)
+{
 }
 #endif /* FEATURE_DP_DAL_SIM */
--- dp/wifi3.0/dal/dp_dal_offload_sim.h
+++ dp/wifi3.0/dal/dp_dal_offload_sim.h
@@ -20,6 +20,48 @@ enum offload_sim_ring_type {
 };
 
 /**
+ * struct offload_sim_irq_ctx - IRQ context for interrupt handler
+ * @dal_sim_ctx: Pointer to DAL simulation context
+ * @ring_id: Ring ID (0 or 1)
+ * @ring_type: Ring type (RX or TX completion)
+ * @irq_configured: Flag indicating if IRQ has been successfully configured
+ */
+struct offload_sim_irq_ctx {
+	struct dp_dal_sim_ctx *dal_sim_ctx;
+	uint8_t ring_id;
+	enum offload_sim_ring_type ring_type;
+	bool irq_configured;
+};
+
+/**
+ * struct dp_dal_offload_sim_ctx - Offload simulation context
+ * @hal_soc: Vendor HAL SoC structure
+ * @rx_ring_hal_srng: Array of vendor HAL SRNG structures for RX rings
+ * @tx_cmpl_ring_hal_srng: Array of vendor HAL SRNG structures for
+ * TX completion rings
+ * @tx_ring_hal_srng: Array of vendor HAL SRNG structures for TX rings
+ * @rx_refill_ring_hal_srng: Vendor HAL SRNG structure for RX refill ring
+ * @rx_irq_ctx: IRQ context for RX rings
+ * @tx_cpl_irq_ctx: IRQ context for TX completion rings
+ * @offload_sim_ctx_initialized: Flag indicating if context is initialized
+ * @dev_base_addr: device base address
+ *
+ * This structure maintains the offload simulation context with vendor HAL
+ * ring structures that are populated from dal_sim_srng structures.
+ */
+struct dp_dal_offload_sim_ctx {
+	struct dal_vndr_hal_soc hal_soc;
+	struct dal_vndr_hal_srng rx_ring_hal_srng[DAL_RX_RINGS_MAX];
+	struct dal_vndr_hal_srng tx_cmpl_ring_hal_srng[DAL_TX_RINGS_MAX];
+	struct dal_vndr_hal_srng tx_ring_hal_srng[DAL_TX_RINGS_MAX];
+	struct dal_vndr_hal_srng rx_refill_ring_hal_srng;
+	struct offload_sim_irq_ctx rx_irq_ctx[DAL_RX_RINGS_MAX];
+	struct offload_sim_irq_ctx tx_cpl_irq_ctx[DAL_TX_RINGS_MAX];
+	bool offload_sim_ctx_initialized;
+	void *dev_base_addr;
+};
+
+/**
  * dp_dal_offload_sim_init() - Initialize offload simulation context
  * @dal_sim_ctx: Pointer to DAL simulation context
  *
@@ -38,5 +80,12 @@ int dp_dal_offload_sim_init(struct dp_dal_sim_ctx *dal_sim_ctx);
  * This function deinitializes and frees the offload simulation context.
  */
 void dp_dal_offload_sim_deinit(struct dp_dal_sim_ctx *dal_sim_ctx);
+/**
+ * dp_dal_offload_sim_free_irq() - Free IRQs for offload simulation
+ * @dal_sim_ctx: Pointer to DAL simulation context
+ *
+ * This function frees interrupt handlers for RX and TX completion rings.
+ */
+void dp_dal_offload_sim_free_irq(struct dp_dal_sim_ctx *dal_sim_ctx);
 #endif /* FEATURE_DAL_DP_SUPPORT */
 #endif /* DP_DAL_OFFLOAD_SIM_H */
