a38dde666fcbded0a026476fc7aa8647f38b2f31
Arsalan Sariya <asariya@qti.qualcomm.com>
2025-11-18 01:01:52 +0530

    qcacmn: Add apis to call hal ops
    Add apis to call dal vendor hal ops.
    
    Change-Id: I01f0f0bfddd9ddc06c4652c535bf3d0f7d06f2bf
    CRs-Fixed: 4356144

--- hal/wifi3.0/dal/dal_vndr_hal_api.h
+++ hal/wifi3.0/dal/dal_vndr_hal_api.h
@@ -256,6 +256,29 @@ static inline void dal_vndr_hal_srng_access_end(
 }
 
 /**
+ * dal_vndr_hal_rxdma_buff_addr_info_set() - set the buffer_addr_info of the
+ *				    rxdma ring entry.
+ * @hal_soc_hdl: hal soc handle
+ * @rxdma_entry: descriptor entry
+ * @paddr: physical address of nbuf data pointer.
+ * @cookie: SW cookie used as a index to SW rx desc.
+ * @manager: who owns the nbuf (host, NSS, etc...).
+ *
+ */
+static inline void dal_vndr_hal_rxdma_buff_addr_info_set(
+					void *hal_soc_hdl,
+					void *rxdma_entry,
+					dma_addr_t paddr,
+					uint32_t cookie,
+					uint8_t manager)
+{
+	struct dal_vndr_hal_soc *hal_soc =
+		(struct dal_vndr_hal_soc *)hal_soc_hdl;
+	hal_soc->ops->dal_vndr_hal_rxdma_buff_addr_info_set(rxdma_entry,
+							    paddr,
+							    cookie, manager);
+}
+/**
  * dal_vndr_hal_tx_desc_sync() - Commit the descriptor to Hardware
  * @hal_tx_desc_cached: Cached descriptor that software maintains
  * @hw_desc: Hardware descriptor to be updated
@@ -292,6 +315,492 @@ static inline void dal_vndr_hal_srng_dst_dec_tp(
 		srng->u.dst_ring.tp = (srng->ring_size - srng->entry_size);
 	else
 		srng->u.dst_ring.tp -= srng->entry_size;
+}
+
+/**
+ * dal_vndr_hal_tx_desc_set_lmac_id() - Set LMAC ID in Tx descriptor
+ * @hal_soc_hdl: HAL SoC handle
+ * @desc: Tx descriptor pointer
+ * @lmac_id: LMAC ID value
+ *
+ * Return: void
+ */
+static inline void dal_vndr_hal_tx_desc_set_lmac_id(
+				void *hal_soc_hdl,
+				void *desc, uint8_t lmac_id)
+{
+	struct dal_vndr_hal_soc *hal_soc =
+				(struct dal_vndr_hal_soc *)hal_soc_hdl;
+
+	hal_soc->ops->dal_vndr_hal_tx_desc_set_lmac_id(desc, lmac_id);
+}
+
+/**
+ * dal_vndr_hal_tx_desc_set_buf_addr() - Set buffer address in Tx descriptor
+ * @hal_soc_hdl: HAL SoC handle
+ * @desc: Tx descriptor pointer
+ * @paddr: Physical buffer address
+ * @rbm_id: rbm ID
+ * @desc_id: Descriptor ID
+ * @type: Buffer type
+ *
+ * Return: void
+ */
+static inline void dal_vndr_hal_tx_desc_set_buf_addr(
+				void *hal_soc_hdl,
+				void *desc, dma_addr_t paddr,
+				uint8_t rbm_id, uint32_t desc_id,
+				uint8_t type)
+{
+	struct dal_vndr_hal_soc *hal_soc =
+					(struct dal_vndr_hal_soc *)hal_soc_hdl;
+
+	hal_soc->ops->dal_vndr_hal_tx_desc_set_buf_addr(desc, paddr, rbm_id,
+							desc_id, type);
+}
+
+/**
+ * dal_vndr_hal_tx_desc_set_buf_length() - Set buffer length in Tx descriptor
+ * @hal_soc_hdl: HAL SoC handle
+ * @desc: Tx descriptor pointer
+ * @data_length: Buffer data length
+ *
+ * Return: void
+ */
+static inline void dal_vndr_hal_tx_desc_set_buf_length(
+				void *hal_soc_hdl,
+				void *desc, uint16_t data_length)
+{
+	struct dal_vndr_hal_soc *hal_soc =
+				(struct dal_vndr_hal_soc *)hal_soc_hdl;
+
+	hal_soc->ops->dal_vndr_hal_tx_desc_set_buf_length(desc, data_length);
+}
+
+/**
+ * dal_vndr_hal_tx_desc_set_buf_offset() - Set buffer offset in Tx descriptor
+ * @hal_soc_hdl: HAL SoC handle
+ * @desc: Tx descriptor pointer
+ * @offset: Buffer offset value
+ *
+ * Return: void
+ */
+static inline void dal_vndr_hal_tx_desc_set_buf_offset(
+				void *hal_soc_hdl, void *desc, uint8_t offset)
+{
+	struct dal_vndr_hal_soc *hal_soc =
+				(struct dal_vndr_hal_soc *)hal_soc_hdl;
+
+	hal_soc->ops->dal_vndr_hal_tx_desc_set_buf_offset(desc, offset);
+}
+
+/**
+ * dal_vndr_hal_tx_desc_set_l3_checksum_en() - Enable L3 checksum offload
+ * @hal_soc_hdl: HAL SoC handle
+ * @desc: Tx descriptor pointer
+ * @en: Enable flag (1 = enable, 0 = disable)
+ *
+ * Return: void
+ */
+static inline void dal_vndr_hal_tx_desc_set_l3_checksum_en(
+			void *hal_soc_hdl, void *desc, uint8_t en)
+{
+	struct dal_vndr_hal_soc *hal_soc =
+				(struct dal_vndr_hal_soc *)hal_soc_hdl;
+
+	hal_soc->ops->dal_vndr_hal_tx_desc_set_l3_checksum_en(desc, en);
+}
+
+/**
+ * dal_vndr_hal_tx_desc_set_l4_checksum_en() - Enable L4 checksum offload
+ * @hal_soc_hdl: HAL SoC handle
+ * @desc: Tx descriptor pointer
+ * @en: Enable flag (1 = enable, 0 = disable)
+ *
+ * Return: void
+ */
+static inline void dal_vndr_hal_tx_desc_set_l4_checksum_en(
+				void *hal_soc_hdl, void *desc, uint8_t en)
+{
+	struct dal_vndr_hal_soc *hal_soc =
+				(struct dal_vndr_hal_soc *)hal_soc_hdl;
+
+	hal_soc->ops->dal_vndr_hal_tx_desc_set_l4_checksum_en(desc, en);
+}
+
+/**
+ * dal_vndr_hal_tx_desc_set_bank_id() - Set bank ID in Tx descriptor
+ * @hal_soc_hdl: HAL SoC handle
+ * @desc: Tx descriptor pointer
+ * @bank_id: Bank ID value
+ *
+ * Return: void
+ */
+static inline void dal_vndr_hal_tx_desc_set_bank_id(
+			void *hal_soc_hdl, void *desc, uint8_t bank_id)
+{
+	struct dal_vndr_hal_soc *hal_soc =
+				(struct dal_vndr_hal_soc *)hal_soc_hdl;
+
+	hal_soc->ops->dal_vndr_hal_tx_desc_set_bank_id(desc, bank_id);
+}
+
+/**
+ * dal_vndr_hal_tx_desc_set_vdev_id() - Set VDEV ID in Tx descriptor
+ * @hal_soc_hdl: HAL SoC handle
+ * @desc: Tx descriptor pointer
+ * @vdev_id: VDEV ID value
+ *
+ * Return: void
+ */
+static inline void dal_vndr_hal_tx_desc_set_vdev_id(
+			void *hal_soc_hdl, void *desc, uint8_t vdev_id)
+{
+	struct dal_vndr_hal_soc *hal_soc =
+				(struct dal_vndr_hal_soc *)hal_soc_hdl;
+
+	hal_soc->ops->dal_vndr_hal_tx_desc_set_vdev_id(desc, vdev_id);
+}
+
+/**
+ * dal_vndr_hal_tx_desc_set_hlos_tid() - Set HLOS TID in Tx descriptor
+ * @hal_soc_hdl: HAL SoC handle
+ * @desc: Tx descriptor pointer
+ * @hlos_tid: HLOS TID value
+ *
+ * Return: void
+ */
+static inline void dal_vndr_hal_tx_desc_set_hlos_tid(
+			void *hal_soc_hdl, void *desc, uint8_t hlos_tid)
+{
+	struct dal_vndr_hal_soc *hal_soc =
+				(struct dal_vndr_hal_soc *)hal_soc_hdl;
+
+	hal_soc->ops->dal_vndr_hal_tx_desc_set_hlos_tid(desc, hlos_tid);
+}
+
+/**
+ * dal_vndr_hal_tx_desc_set_flow_override_enable() - Enable flow override
+ * @hal_soc_hdl: HAL SoC handle
+ * @desc: Tx descriptor pointer
+ * @val: Enable value
+ *
+ * Return: void
+ */
+static inline void dal_vndr_hal_tx_desc_set_flow_override_enable(
+				void *hal_soc_hdl, void *desc, uint8_t val)
+{
+	struct dal_vndr_hal_soc *hal_soc =
+				(struct dal_vndr_hal_soc *)hal_soc_hdl;
+
+	hal_soc->ops->dal_vndr_hal_tx_desc_set_flow_override_enable(desc, val);
+}
+
+/**
+ * dal_vndr_hal_tx_desc_set_flow_override() - Set flow override value
+ * @hal_soc_hdl: HAL SoC handle
+ * @desc: Tx descriptor pointer
+ * @val: Flow override value
+ *
+ * Return: void
+ */
+static inline void dal_vndr_hal_tx_desc_set_flow_override(
+				void *hal_soc_hdl, void *desc, uint8_t val)
+{
+	struct dal_vndr_hal_soc *hal_soc =
+				(struct dal_vndr_hal_soc *)hal_soc_hdl;
+
+	hal_soc->ops->dal_vndr_hal_tx_desc_set_flow_override(desc, val);
+}
+
+/**
+ * dal_vndr_hal_tx_desc_set_who_classify_info_sel() - Set WHO classify info
+ * selector
+ * @hal_soc_hdl: HAL SoC handle
+ * @desc: Tx descriptor pointer
+ * @val: WHO classify info selector value
+ *
+ * Return: void
+ */
+static inline void dal_vndr_hal_tx_desc_set_who_classify_info_sel(
+				void *hal_soc_hdl, void *desc, uint8_t val)
+{
+	struct dal_vndr_hal_soc *hal_soc =
+				(struct dal_vndr_hal_soc *)hal_soc_hdl;
+
+	hal_soc->ops->dal_vndr_hal_tx_desc_set_who_classify_info_sel(desc, val);
+}
+
+/**
+ * dal_vndr_hal_tx_desc_set_tx_notify_frame() - Set Tx notify frame flag
+ * @hal_soc_hdl: HAL SoC handle
+ * @desc: Tx descriptor pointer
+ * @val: Notify frame flag value
+ *
+ * Return: void
+ */
+static inline void dal_vndr_hal_tx_desc_set_tx_notify_frame(
+				void *hal_soc_hdl, void *desc, uint8_t val)
+{
+	struct dal_vndr_hal_soc *hal_soc =
+				(struct dal_vndr_hal_soc *)hal_soc_hdl;
+
+	hal_soc->ops->dal_vndr_hal_tx_desc_set_tx_notify_frame(desc, val);
+}
+
+/**
+ * dal_vndr_hal_tx_comp_get_status() - Get Tx completion status
+ * @hal_soc_hdl: HAL SoC handle
+ * @desc: Tx completion descriptor pointer
+ * @ts1: Tx status structure pointer
+ *
+ * Return: void
+ */
+static inline void dal_vndr_hal_tx_comp_get_status(
+			void *hal_soc_hdl, void *desc, void *ts1)
+{
+	struct dal_vndr_hal_soc *hal_soc =
+				(struct dal_vndr_hal_soc *)hal_soc_hdl;
+
+	hal_soc->ops->dal_vndr_hal_tx_comp_get_status(desc, ts1);
+}
+
+/**
+ * dal_vndr_hal_rx_error_status_get() - Get Rx error status
+ * @hal_soc_hdl: HAL SoC handle
+ * @reo_desc: REO descriptor pointer
+ *
+ * Return: Error status value
+ */
+static inline uint8_t dal_vndr_hal_rx_error_status_get(
+			void *hal_soc_hdl, void *reo_desc)
+{
+	struct dal_vndr_hal_soc *hal_soc =
+				(struct dal_vndr_hal_soc *)hal_soc_hdl;
+
+	return hal_soc->ops->dal_vndr_hal_rx_error_status_get(reo_desc);
+}
+
+/**
+ * dal_vndr_hal_rx_reo_buf_cookie_get() - Get REO buffer cookie
+ * @hal_soc_hdl: HAL SoC handle
+ * @reo_desc: REO descriptor pointer
+ *
+ * Return: Buffer cookie value
+ */
+static inline uint32_t dal_vndr_hal_rx_reo_buf_cookie_get(
+			void *hal_soc_hdl, void *reo_desc)
+{
+	struct dal_vndr_hal_soc *hal_soc =
+				(struct dal_vndr_hal_soc *)hal_soc_hdl;
+
+	return hal_soc->ops->dal_vndr_hal_rx_reo_buf_cookie_get(reo_desc);
+}
+
+/**
+ * dal_vndr_hal_rx_ret_buf_manager_get() - Get return buffer manager ID
+ * @hal_soc_hdl: HAL SoC handle
+ * @ring_desc: Ring descriptor pointer
+ *
+ * Return: Buffer manager ID
+ */
+static inline uint8_t dal_vndr_hal_rx_ret_buf_manager_get(
+				void *hal_soc_hdl, void *ring_desc)
+{
+	struct dal_vndr_hal_soc *hal_soc =
+				(struct dal_vndr_hal_soc *)hal_soc_hdl;
+
+	return hal_soc->ops->dal_vndr_hal_rx_ret_buf_manager_get(ring_desc);
+}
+
+/**
+ * dal_vndr_hal_rx_reo_get_details() - Get REO buffer details
+ * @hal_soc_hdl: HAL SoC handle
+ * @ring_desc: Ring descriptor pointer
+ * @buf_info: Buffer info structure pointer to be filled
+ *
+ * Return: void
+ */
+static inline void dal_vndr_hal_rx_reo_get_details(
+			void *hal_soc_hdl, void *ring_desc,
+			struct dal_vndr_hal_buf_info *buf_info)
+{
+	struct dal_vndr_hal_soc *hal_soc =
+				(struct dal_vndr_hal_soc *)hal_soc_hdl;
+
+	hal_soc->ops->dal_vndr_hal_rx_reo_get_details(ring_desc, buf_info);
+}
+
+/**
+ * dal_vndr_hal_rx_msdu_start_nss_get() - Get NSS from MSDU start
+ * @hal_soc_hdl: HAL SoC handle
+ * @buf: Buffer pointer
+ *
+ * Return: NSS value
+ */
+static inline uint32_t dal_vndr_hal_rx_msdu_start_nss_get(void *hal_soc_hdl,
+							  uint8_t *buf)
+{
+	struct dal_vndr_hal_soc *hal_soc =
+				(struct dal_vndr_hal_soc *)hal_soc_hdl;
+
+	return hal_soc->ops->dal_vndr_hal_rx_msdu_start_nss_get(buf);
+}
+
+/**
+ * dal_vndr_hal_rx_tlv_sgi_get() - Get SGI from Rx TLV
+ * @hal_soc_hdl: HAL SoC handle
+ * @buf: Buffer pointer
+ *
+ * Return: SGI value
+ */
+static inline uint32_t dal_vndr_hal_rx_tlv_sgi_get(
+				void *hal_soc_hdl, uint8_t *buf)
+{
+	struct dal_vndr_hal_soc *hal_soc =
+				(struct dal_vndr_hal_soc *)hal_soc_hdl;
+
+	return hal_soc->ops->dal_vndr_hal_rx_tlv_sgi_get(buf);
+}
+
+/**
+ * dal_vndr_hal_rx_tlv_rate_mcs_get() - Get rate MCS from Rx TLV
+ * @hal_soc_hdl: HAL SoC handle
+ * @buf: Buffer pointer
+ *
+ * Return: Rate MCS value
+ */
+static inline uint32_t dal_vndr_hal_rx_tlv_rate_mcs_get(
+				void *hal_soc_hdl, uint8_t *buf)
+{
+	struct dal_vndr_hal_soc *hal_soc =
+				(struct dal_vndr_hal_soc *)hal_soc_hdl;
+
+	return hal_soc->ops->dal_vndr_hal_rx_tlv_rate_mcs_get(buf);
+}
+
+/**
+ * dal_vndr_hal_rx_tlv_bw_get() - Get bandwidth from Rx TLV
+ * @hal_soc_hdl: HAL SoC handle
+ * @buf: Buffer pointer
+ *
+ * Return: Bandwidth value
+ */
+static inline uint32_t dal_vndr_hal_rx_tlv_bw_get(
+				void *hal_soc_hdl, uint8_t *buf)
+{
+	struct dal_vndr_hal_soc *hal_soc =
+				(struct dal_vndr_hal_soc *)hal_soc_hdl;
+
+	return hal_soc->ops->dal_vndr_hal_rx_tlv_bw_get(buf);
+}
+
+/**
+ * dal_vndr_hal_rx_tlv_get_pkt_type() - Get packet type from Rx TLV
+ * @hal_soc_hdl: HAL SoC handle
+ * @buf: Buffer pointer
+ *
+ * Return: Packet type value
+ */
+static inline uint32_t dal_vndr_hal_rx_tlv_get_pkt_type(
+				void *hal_soc_hdl, uint8_t *buf)
+{
+	struct dal_vndr_hal_soc *hal_soc =
+				 (struct dal_vndr_hal_soc *)hal_soc_hdl;
+
+	return hal_soc->ops->dal_vndr_hal_rx_tlv_get_pkt_type(buf);
+}
+
+/**
+ * dal_vndr_hal_rx_get_l3_pad_bytes() - Get L3 padding bytes
+ * @hal_soc_hdl: HAL SoC handle
+ * @buf: Buffer pointer
+ *
+ * Return: L3 padding bytes value
+ */
+static inline uint8_t dal_vndr_hal_rx_get_l3_pad_bytes(
+				void *hal_soc_hdl, uint8_t *buf)
+{
+	struct dal_vndr_hal_soc *hal_soc =
+				(struct dal_vndr_hal_soc *)hal_soc_hdl;
+
+	return hal_soc->ops->dal_vndr_hal_rx_get_l3_pad_bytes(buf);
+}
+
+/**
+ * dal_vndr_hal_rx_tlv_msdu_done_get() - Get MSDU done flag from Rx TLV
+ * @hal_soc_hdl: HAL SoC handle
+ * @buf: Buffer pointer
+ *
+ * Return: MSDU done flag value
+ */
+static inline uint32_t dal_vndr_hal_rx_tlv_msdu_done_get(
+				void *hal_soc_hdl, uint8_t *buf)
+{
+	struct dal_vndr_hal_soc *hal_soc =
+				(struct dal_vndr_hal_soc *)hal_soc_hdl;
+
+	return hal_soc->ops->dal_vndr_hal_rx_tlv_msdu_done_get(buf);
+}
+
+/**
+ * dal_vndr_hal_rx_reo_buf_paddr_get() - Get REO buffer physical address
+ * @hal_soc_hdl: HAL SoC handle
+ * @ring_desc: Ring descriptor pointer
+ *
+ * Return: Buffer physical address
+ */
+static inline dma_addr_t dal_vndr_hal_rx_reo_buf_paddr_get(void *hal_soc_hdl,
+							   void *ring_desc)
+{
+	struct dal_vndr_hal_soc *hal_soc =
+				(struct dal_vndr_hal_soc *)hal_soc_hdl;
+
+	return hal_soc->ops->dal_vndr_hal_rx_reo_buf_paddr_get(ring_desc);
+}
+
+/**
+ * dal_vndr_hal_tx_comp_get_paddr() - Get physical address from Tx
+ * completion descriptor
+ * @hal_soc_hdl: HAL SoC handle
+ * @hal_desc: Tx completion descriptor pointer
+ *
+ * This function extracts the buffer physical address from the hardware
+ * completion descriptor.
+ *
+ * Return: Buffer physical address
+ */
+static inline dma_addr_t dal_vndr_hal_tx_comp_get_paddr(void *hal_soc_hdl,
+							void *hal_desc)
+{
+	struct dal_vndr_hal_soc *hal_soc =
+				(struct dal_vndr_hal_soc *)hal_soc_hdl;
+
+	return hal_soc->ops->dal_vndr_hal_tx_comp_get_paddr(hal_desc);
+}
+
+/**
+ * dal_vndr_hal_rx_msdu_desc_info_get() - Get MSDU descriptor info from REO
+ * ring descriptor
+ * @hal_soc_hdl: HAL SoC handle
+ * @desc_addr: REO ring descriptor address
+ * @msdu_desc_info: Pointer to structure to be filled with MSDU descriptor info
+ *
+ * This function extracts MSDU descriptor information including flags
+ * (first_msdu_in_mpdu, last_msdu_in_mpdu, msdu_continuation) and MSDU length
+ * from the REO destination ring descriptor.
+ *
+ * Return: void
+ */
+static inline void dal_vndr_hal_rx_msdu_desc_info_get(
+			void *hal_soc_hdl, void *desc_addr,
+			struct dal_vndr_hal_rx_msdu_desc_info *msdu_desc_info)
+{
+	struct dal_vndr_hal_soc *hal_soc =
+			(struct dal_vndr_hal_soc *)hal_soc_hdl;
+
+	hal_soc->ops->dal_vndr_hal_rx_msdu_desc_info_get(desc_addr,
+							 msdu_desc_info);
 }
 
 /**
