72a98c7b5e9ac118cdf21c9aa40f36090a35fabc
Aasir Rasheed <arasheed@qti.qualcomm.com>
2025-12-15 19:43:50 -0800

    qcacmn: Ensure accurate PHY mode configuration for 2.4/5/6 GHz bands
    The host driver currently uses MODE_11NA_HT20 for all bands,
    which results in CFR capture failures.
    
    For CFR RX on unassociated clients,
    the following PHY modes should be applied:
    
    2.4 GHz: MODE_11NG_HT20
    5 GHz: MODE_11NA_HT20
    6 GHz: MODE_11AX_HE20
    
    This change ensures proper PHY mode configuration per band.
    
    Change-Id: Ice26ae4b3fc650d690c26c9fdfa4fdf765b7c69d
    CRs-Fixed: 4373528

--- target_if/cfr/src/target_if_cfr.c
+++ target_if/cfr/src/target_if_cfr.c
@@ -895,11 +895,32 @@ static uint8_t target_if_cfr_get_pdev_id(struct wlan_objmgr_pdev *pdev)
 }
 #endif /* QCA_WIFI_QCA6490 || QCA_WIFI_KIWI */
 
+static enum
+wlan_phymode target_if_cfr_get_rx_phy_mode(int32_t freq)
+{
+	enum reg_wifi_band cur_band;
+
+	cur_band = wlan_reg_freq_to_band(freq);
+
+	switch (cur_band) {
+	case REG_BAND_2G:
+		return WLAN_PHYMODE_11NG_HT20;
+	case REG_BAND_5G:
+		return WLAN_PHYMODE_11NA_HT20;
+	case REG_BAND_6G:
+		return WLAN_PHYMODE_11AXA_HE20;
+	default:
+		cfr_err("Invalid band %d", cur_band);
+		return WLAN_PHYMODE_11NA_HT20;
+	}
+}
+
 QDF_STATUS target_if_cfr_config_rcc(struct wlan_objmgr_pdev *pdev,
 				    struct cfr_rcc_param *rcc_info)
 {
 	QDF_STATUS status;
 	struct wmi_unified *pdev_wmi_handle = NULL;
+	int32_t chan_freq;
 
 	pdev_wmi_handle = lmac_get_pdev_wmi_handle(pdev);
 	if (!pdev_wmi_handle) {
@@ -910,6 +931,12 @@ QDF_STATUS target_if_cfr_config_rcc(struct wlan_objmgr_pdev *pdev,
 	rcc_info->pdev_id = target_if_cfr_get_pdev_id(pdev);
 	rcc_info->num_grp_tlvs =
 		count_set_bits(&rcc_info->modified_in_curr_session[0]);
+
+	chan_freq = rcc_info->unassoc_channel_mhz;
+	if (chan_freq) {
+		rcc_info->unassoc_phy_mode =
+			target_if_cfr_get_rx_phy_mode(chan_freq);
+	}
 
 	status = wmi_unified_send_cfr_rcc_cmd(pdev_wmi_handle, rcc_info);
 	return status;
