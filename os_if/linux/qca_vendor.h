d3948eb58d154f955eff15178d07452820c21afa
Meng Yuan <menyua@qti.qualcomm.com>
2026-01-09 15:02:46 +0800

    qcacmn: Introduce new vendor for host driver TX/RX counters
    Add new vendor TX/RX counters to retrieve host/driver side specific
    counters and drop counts.
    
    Standard interfaces (e.g., station dump) typically report aggregated
    HW+SW statistics. This interface exposes separate Host-side counters
    to allow userspace to obtain Host-level specific data, distinguished
    from Firmware/Hardware statistics.
    
    Change-Id: Ie1218e1748985582be251352a76138240ac327c0
    Signed-off-by: Meng Yuan <menyua@qti.qualcomm.com>

--- os_if/linux/qca_vendor.h
+++ os_if/linux/qca_vendor.h
@@ -566,6 +566,11 @@
  *
  *	The attributes used with this command are defined in
  *	enum qca_wlan_vendor_attr_mcc_quota.
+ *
+ * @QCA_NL80211_VENDOR_SUBCMD_WLAN_HOST_TXRX_STATS: This vendor subcommand is
+ *     used to retrieve WLAN host driver side TX/RX statistics.
+ *     The attributes used with this command are defined in
+ *     enum qca_wlan_host_txrx_stats_attr.
  */
 
 enum qca_nl80211_vendor_subcmds {
@@ -806,6 +811,7 @@ enum qca_nl80211_vendor_subcmds {
 	QCA_NL80211_VENDOR_SUBCMD_SET_MONITOR_MODE = 202,
 	QCA_NL80211_VENDOR_SUBCMD_ROAM_EVENTS = 203,
 	QCA_NL80211_VENDOR_SUBCMD_MCC_QUOTA = 205,
+	QCA_NL80211_VENDOR_SUBCMD_WLAN_HOST_TXRX_STATS = 270,
 };
 
 enum qca_wlan_vendor_tos {
@@ -12878,6 +12884,44 @@ enum qca_wlan_vendor_opm_mode {
 	QCA_WLAN_VENDOR_OPM_MODE_DISABLE = 0,
 	QCA_WLAN_VENDOR_OPM_MODE_ENABLE = 1,
 	QCA_WLAN_VENDOR_OPM_MODE_USER_DEFINED = 2,
+};
+
+/**
+ * enum qca_wlan_host_txrx_stats_attr - Defines attributes to be used
+ * with vendor subcmd QCA_NL80211_VENDOR_SUBCMD_WLAN_HOST_TXRX_STATS.
+ *
+ * @QCA_WLAN_VENDOR_ATTR_HOST_TXRX_STATS_PARAM_TX_PKTS: 32-bit unsigned value
+ *	for total TX packets successfully transmitted to the firmware.
+ *
+ * @QCA_WLAN_VENDOR_ATTR_HOST_TXRX_STATS_PARAM_TX_DROPPED: 32-bit unsigned
+ *	value for total TX packets dropped by the WLAN host driver (e.g., due
+ *	to queue exhaustion).
+ *
+ * @QCA_WLAN_VENDOR_ATTR_HOST_TXRX_STATS_PARAM_RX_PKTS: 32-bit unsigned value
+ *	for total RX packets successfully delivered to the network stack.
+ *
+ * @QCA_WLAN_VENDOR_ATTR_HOST_TXRX_STATS_PARAM_RX_DROPPED: 32-bit unsigned
+ *	value for total RX packets dropped by the WLAN host driver.
+ *
+ * @QCA_WLAN_VENDOR_ATTR_HOST_TXRX_STATS_PARAM_IPA_EXCEPTION_RX_DROPPED:
+ *	64-bit unsigned value for IPA exception RX drops. Counts packets
+ *	routed from IPA to WLAN driver that were dropped.
+ * @QCA_WLAN_VENDOR_ATTR_HOST_TXRX_STATS_PARAM_PAD: Attribute used for padding
+ *	for 64-bit alignment.
+ */
+
+enum qca_wlan_host_txrx_stats_attr {
+	QCA_WLAN_VENDOR_ATTR_HOST_TXRX_STATS_PARAM_INVALID          = 0,
+	QCA_WLAN_VENDOR_ATTR_HOST_TXRX_STATS_PARAM_TX_PKTS    = 1,
+	QCA_WLAN_VENDOR_ATTR_HOST_TXRX_STATS_PARAM_TX_DROPPED = 2,
+	QCA_WLAN_VENDOR_ATTR_HOST_TXRX_STATS_PARAM_RX_PKTS    = 3,
+	QCA_WLAN_VENDOR_ATTR_HOST_TXRX_STATS_PARAM_RX_DROPPED = 4,
+	QCA_WLAN_VENDOR_ATTR_HOST_TXRX_STATS_PARAM_IPA_EXCEPTION_RX_DROPPE = 5,
+	QCA_WLAN_VENDOR_ATTR_HOST_TXRX_STATS_PARAM_PAD = 6,
+
+	QCA_WLAN_VENDOR_ATTR_HOST_TXRX_STATS_PARAM_LAST,
+	QCA_WLAN_VENDOR_ATTR_HOST_TXRX_STATS_PARAM_MAX =
+		QCA_WLAN_VENDOR_ATTR_HOST_TXRX_STATS_PARAM_LAST - 1
 };
 
 #endif
