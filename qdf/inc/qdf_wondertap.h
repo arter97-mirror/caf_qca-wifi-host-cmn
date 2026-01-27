bef49af46339e581e714ad295d4c233d3854b922
Yeshwanth Sriram Guntuka <ysriramg@qti.qualcomm.com>
2026-01-27 15:17:04 -0800

    qcacmn: Add QDF support for wondertap channel schedule op
    Add QDF wrappers for supporting wondertap channel
    schedule operation.
    
    Change-Id: Ie73ee490c95204ad8c07eab3ff78abc07ed9cddc
    CRs-Fixed: 4453971

--- qdf/inc/qdf_wondertap.h
+++ qdf/inc/qdf_wondertap.h
@@ -116,6 +116,31 @@ typedef __qdf_wondertap_capability_t qdf_wondertap_capability_t;
 typedef __qdf_wonder_txd_t qdf_wonder_txd_t;
 
 /**
+ * typedef qdf_wondertap_role_t - Wondertap role enumerator
+ *
+ * Wondertap data enum that defines role in channel hopping list.
+ */
+typedef __qdf_wondertap_role_t qdf_wondertap_role_t;
+
+/**
+ * typedef qdf_wondertap_chan_list_params_t - Wondertap channel list params
+ *  data structure
+ *
+ * Wondertap data structure that provides channel list parameters such as freq,
+ * bandwidth and role.
+ */
+typedef __qdf_wondertap_chan_list_params_t qdf_wondertap_chan_list_params_t;
+
+/**
+ * typedef qdf_wondertap_channel_sch_req_t - Wondertap channel schedule request
+ *  data structure
+ *
+ * Wondertap data structure that provides channel schedule
+ * request to be followed by vendor driver.
+ */
+typedef __qdf_wondertap_channel_sch_req_t qdf_wondertap_channel_sch_req_t;
+
+/**
  * typedef qdf_wondertap_init_params_t - Wondertap initialization parameters
  *
  * Structure containing all parameters required for wondertap interface
