5f6733ae0a3cc826f1dce55eb2d7168f4e3e29c2
Rakesh Pillai <pillair@qti.qualcomm.com>
2026-01-31 23:19:13 -0800

    qcacmn: Fix the refilled bufs count in replenish path
    Currently when the dynamic resource manager attempts
    to replenish RX buffers, the counter for number of buffers
    replenished is incremented without actually checking if the
    replenish API actually refilled the requested number of buffers.
    
    Fix this by incrementing the rx_bufs_used by the actual
    number of RX buffers refilled by the replenish API.
    
    Change-Id: I36d7f8400c833239be7675a3a7f1cc2978f5b854
    CRs-Fixed: 4424442

--- dp/wifi3.0/dp_rx.h
+++ dp/wifi3.0/dp_rx.h
@@ -4233,10 +4233,12 @@ __dp_rx_buffers_replenish_wrapper(struct dp_soc *soc, uint8_t mac_id,
 	rx_desc_pool = &soc->rx_desc_buf[mac_id];
 	for (i = 0; i < soc->num_replenish_rings[mac_id]; i++) {
 		dp_rxdma_srng = soc->replenish_rings[mac_id][i];
-		dp_rx_buffers_replenish(soc, mac_id, dp_rxdma_srng,
-					rx_desc_pool, rx_bufs_reaped,
-					desc_list, tail, req_only);
-		rx_bufs_used += rx_bufs_reaped;
+		rx_bufs_used += dp_rx_buffers_replenish(soc, mac_id,
+							dp_rxdma_srng,
+							rx_desc_pool,
+							rx_bufs_reaped,
+							desc_list, tail,
+							req_only);
 	}
 
 	return rx_bufs_used;
