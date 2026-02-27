6fc5542951f7d1f1cf4fc81ec185ab4d888bad82
Samarjeet Patel <sampate@qti.qualcomm.com>
2026-02-27 02:02:43 -0800

    qcacmn: Exclude RX REO stats from peer info stats request
    Currently, the peer‑info firmware stats request enables all
    peer statistic TLVs by iterating up to HTT_PEER_STATS_MAX_TLV
    and setting the corresponding bits in the request mask.
    This also includes HTT_PEER_RX_REO_STATS_TLV, which should not
    be requested here. RX REO statistics must be retrieved only
    through the BN path and are intended to be accessed via the
    vendor command.
    This change updates the loop to explicitly check for and exclude
    HTT_PEER_RX_REO_STATS_TLV when constructing the request mask.
    This prevents RX REO stats from being requested during a peer‑info
    stats request.
    
    Change-Id: I6518043e7c7aa5932396131ef80cbb6f24de98e4
    CRs-Fixed: 4468125

--- dp/wifi3.0/dp_main.c
+++ dp/wifi3.0/dp_main.c
@@ -11648,8 +11648,11 @@ dp_fw_stats_process(struct dp_vdev *vdev,
 
 		HTT_DBG_EXT_STATS_PEER_INFO_IS_MAC_ADDR_SET(req->param0, 1);
 
-		for (i = 0; i < HTT_PEER_STATS_MAX_TLV; i++)
-			req->param1 |= (1 << i);
+		for (i = 0; i < HTT_PEER_STATS_MAX_TLV; i++) {
+			if (i != HTT_PEER_RX_REO_STATS_TLV)
+				req->param1 |= (1 << i);
+		}
+
 		req->param2 |= (req->peer_addr[0] & 0x000000ff);
 		req->param2 |= ((req->peer_addr[1] << 8) & 0x0000ff00);
 		req->param2 |= ((req->peer_addr[2] << 16) & 0x00ff0000);
