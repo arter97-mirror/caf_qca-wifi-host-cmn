b39a4ecd5b668e07a3011f5dd4f08b92acde97c2
Hangtian Zhu <hangtian@qti.qualcomm.com>
2026-03-13 19:32:45 +0800

    qca-wifi-oss: fix open() API call
    fix open() API call.
    
    Change-Id: Ib28d3546de598f2e545a0642c8bfc4fb1e4de00a

--- tools/linux/wifi_radar_test_app.c
+++ tools/linux/wifi_radar_test_app.c
@@ -122,7 +122,7 @@ int initialize_wr_dump_file(char *iface)
 	if (tm_val) {
 		strftime(time, 50, "%Y_%m_%d_%T", tm_val);
 		snprintf(filename, sizeof(filename), WR_DUMP_FILE, iface, time);
-		fd = open(filename, O_WRONLY | O_CREAT);
+		fd = open(filename, O_WRONLY | O_CREAT, 0666);
 	} else {
 		perror("Unable to get time value to generate filename\n");
 	}
