83d9a0132118452c98fcc687f856a8f44e74b796
Pragaspathi Thilagaraj <tpragasp@qti.qualcomm.com>
2026-04-20 15:14:01 +0530

    qcacmn: QCA vendor feature flag for PMKSA caching privacy support
    Add a feature flag that allows the driver to indicate PMKSA caching
    privacy support to userspace. This is required when SME is implemented
    in the driver, ensuring that the PMKIDSNonce is included in the
    (Re)Association Request frame whenever PMKSA caching privacy is
    supported by both the AP and non-AP STA.
    
    CRs-Fixed: 4508568
    Change-Id: I9447a1e0852eb7aeea125b85dc0b95ab9cd0c1ea

--- os_if/linux/qca_vendor.h
+++ os_if/linux/qca_vendor.h
@@ -5697,6 +5697,23 @@ enum qca_wlan_vendor_attr_nd_offload {
  * early (using QCA_WLAN_VENDOR_ATTR_P2P_SET_GO_CANCEL_ONE_SHOT_NOA), without
  * waiting for the originally configured NoA duration to expire.
  *
+ * @QCA_WLAN_VENDOR_FEATURE_SUPPORT_PMKSA_CACHING_PRIVACY: Flag indicates that
+ *	the driver supports PMKSA caching privacy as defined in
+ *	IEEE P802.11bi/D4.0, 12.16.7. Userspace should enable PMKSA caching
+ *	privacy only when both %NL80211_EXT_FEATURE_ASSOC_FRAME_ENCRYPTION and
+ *	this feature flag are advertised by the driver.
+ *
+ *	When both the AP and the non-AP STA support PMKSA caching privacy and
+ *	association frame encryption, the driver shall include the PMKIDSNonce
+ *	in the (Re)Association Request frame. Upon successful non-FT
+ *	(re)association, indicated by %NL80211_CMD_CONNECT, %NL80211_CMD_ROAM,
+ *	or %QCA_NL80211_VENDOR_SUBCMD_KEY_MGMT_ROAM_AUTH, wpa_supplicant shall
+ *	derive a new PMKID for the PMKSA entry used during the current
+ *	connection. The derivation uses the PMKIDANonce from the (Re)Association
+ *	Response frame and the PMKIDSNonce from the (Re)Association Request
+ *	frame. PMKR0Name randomization during FT reassociation will be handled
+ *	by the driver.
+ *
  * @NUM_QCA_WLAN_VENDOR_FEATURES: Number of assigned feature bits
  */
 enum qca_wlan_vendor_features {
@@ -5736,6 +5753,7 @@ enum qca_wlan_vendor_features {
 	QCA_WLAN_VENDOR_FEATURE_SUPPORT_P2P_ASSISTED_DFS = 34,
 	QCA_WLAN_VENDOR_FEATURE_SUPPORT_P2P_GO_CANCEL_ONE_SHOT_NOA = 35,
 	QCA_WLAN_VENDOR_FEATURE_SUPPORT_P2P_GC_KEEP_AWAKE_DURING_ONE_SHOT_NOA = 36,
+	QCA_WLAN_VENDOR_FEATURE_SUPPORT_PMKSA_CACHING_PRIVACY = 37,
 	NUM_QCA_WLAN_VENDOR_FEATURES /* keep last */
 };
 
