911932ef3df3f1493cc7deb602c6eadde4f48aca
Swasthikaa J M <swasthik@qti.qualcomm.com>
2026-04-22 16:21:42 +0530

    qcacmn: Fix sizeof mismatch in multi-page allocation
    Use sizeof(*pages->cacheable_pages) instead of
    sizeof(pages->cacheable_pages) to correctly measure array
    element size.
    
    Change-Id: I1687252389787753c2c8afaf08a4d3ac482d3aa4
    CRs-Fixed: 4512189

--- qdf/linux/src/qdf_mem.c
+++ qdf/linux/src/qdf_mem.c
@@ -1745,7 +1745,7 @@ void qdf_mem_multi_pages_alloc_debug(qdf_device_t osdev,
 	if (cacheable) {
 		/* Pages information storage */
 		pages->cacheable_pages = qdf_mem_malloc_debug(
-			pages->num_pages * sizeof(pages->cacheable_pages),
+			pages->num_pages * sizeof(*pages->cacheable_pages),
 			func, line, caller, 0);
 		if (!pages->cacheable_pages)
 			goto out_fail;
@@ -1906,7 +1906,7 @@ void qdf_mem_multi_pages_alloc(qdf_device_t osdev,
 	if (cacheable) {
 		/* Pages information storage */
 		pages->cacheable_pages = qdf_mem_malloc(
-			pages->num_pages * sizeof(pages->cacheable_pages));
+			pages->num_pages * sizeof(*pages->cacheable_pages));
 		if (!pages->cacheable_pages)
 			goto out_fail;
 
@@ -1999,7 +1999,7 @@ void qdf_mem_multi_pages_alloc(qdf_device_t osdev,
 	if (cacheable) {
 		/* Pages information storage */
 		pages->cacheable_pages = qdf_mem_malloc(
-			pages->num_pages * sizeof(pages->cacheable_pages));
+			pages->num_pages * sizeof(*pages->cacheable_pages));
 		if (!pages->cacheable_pages)
 			goto out_fail;
 
