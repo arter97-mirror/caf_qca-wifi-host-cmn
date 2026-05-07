4c4809a86ade588ce83fdce85836c7fb4b13bc25
Amit Mehta <amitmeht@qti.qualcomm.com>
2026-05-07 02:22:54 -0700

    qcacmn: Fix DHCP Bcast Rx connectivity log skip
    Currently qdf_skip_wlan_connectivity_log() skips logging for all
    DHCP broadcast Rx packets, even if they are responses to a DHCP
    REQUEST sent by the DUT. This causes DHCP OFFER and DHCP ACK
    packets to be silently dropped from connectivity logs when they
    arrive as broadcast frames.
    
    Fix this by tracking the DHCP transaction ID from the last TX
    DHCP packet and comparing it against the transaction ID in
    incoming broadcast DHCP Rx packets. Only skip logging if the
    transaction ID does not match (i.e., the broadcast packet is
    not a response to the DUT's own DHCP request).
    
    Change-Id: Ic900c8b3005593472d0a356ba38433419832ed8a
    CRs-Fixed: 4527761

--- qdf/inc/qdf_trace.h
+++ qdf/inc/qdf_trace.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2014-2021 The Linux Foundation. All rights reserved.
- * Copyright (c) 2022-2025 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -942,12 +942,13 @@ void qdf_dp_set_no_of_record(uint32_t val);
  * @dir: direction
  * @pdev_id: pdev_id
  * @op_mode: Vdev Operation mode
+ * @dhcp_ltxid: Last DHCP tx packet transaction ID
  *
  * Return: true: some protocol was logged, false: no protocol was logged.
  */
 bool qdf_dp_trace_log_pkt(uint8_t vdev_id, struct sk_buff *skb,
 			  enum qdf_proto_dir dir, uint8_t pdev_id,
-			  enum QDF_OPMODE op_mode);
+			  enum QDF_OPMODE op_mode, uint32_t dhcp_ltxid);
 
 /**
  * qdf_dp_trace_init() - enables the DP trace
@@ -1369,7 +1370,7 @@ void qdf_dp_track_noack_check(qdf_nbuf_t nbuf, enum qdf_proto_subtype *subtype);
 static inline
 bool qdf_dp_trace_log_pkt(uint8_t vdev_id, struct sk_buff *skb,
 			  enum qdf_proto_dir dir, uint8_t pdev_id,
-			  enum QDF_OPMODE op_mode)
+			  enum QDF_OPMODE op_mode, uint32_t dhcp_ltxid)
 {
 	return false;
 }
