e0e348bac51412d6fdbf8b82af1194a701d9fa74
s1geethb <gbandla@qti.qualcomm.com>
2026-05-20 08:05:37 +0530

    qcacmn: Dual STA IPA iface setup
    In Dual STA mode, an already connected STA1 can cause STA2 to be
    misdetected as a roaming connection. Check session_id before taking
    the roaming path so the second STA interface is handled correctly.
    
    Change-Id: Ibd71d6897645b3442f9915278e8199d7fc5713c2
    CRs-Fixed: 4583880

--- ipa/core/src/wlan_ipa_core.c
+++ ipa/core/src/wlan_ipa_core.c
@@ -3989,7 +3989,8 @@ static QDF_STATUS __wlan_ipa_wlan_evt(qdf_netdev_t net_dev, uint8_t device_mode,
 		 * This is Roaming scenario, so do not need to reset ipa for
 		 * roaming scenario.
 		 */
-		if (ipa_ctx->sta_connected) {
+		if (ipa_ctx->sta_connected &&
+		    ipa_ctx->vdev_to_iface[session_id] != WLAN_IPA_MAX_SESSION) {
 			ipa_info("IPA Roaming event detected from BSSID: "QDF_MAC_ADDR_FMT
 				 " -> BSSID: "QDF_MAC_ADDR_FMT,
 				 QDF_MAC_ADDR_REF(ipa_ctx->iface_context[wlan_ipa_get_ifaceid(
