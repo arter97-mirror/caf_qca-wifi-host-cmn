06a684e47dcff193125dab850e10d438b7670f2a
Arsalan Sariya <asariya@qti.qualcomm.com>
2026-06-04 11:15:02 +0530

    qcacmn: Enable history records for datapath CE
    Enable history records for copy engine owned by datapath.
    The records are enabled for specific events only.
    
    Change-Id: I5f7a31359e441151032c43601a0e65ec2c1557be
    CRs-Fixed: 4229535

--- hif/src/hif_main.h
+++ hif/src/hif_main.h
@@ -181,12 +181,12 @@ struct hif_latency_detect {
 	qdf_time_t credit_report_time;
 };
 #endif
-
 /*
  * Note: For MCL, #if defined (HIF_CONFIG_SLUB_DEBUG_ON) needs to be checked
  * for defined here
  */
-#if defined(HIF_CONFIG_SLUB_DEBUG_ON) || defined(HIF_CE_DEBUG_DATA_BUF)
+#if defined(HIF_CONFIG_SLUB_DEBUG_ON) || defined(HIF_CE_DEBUG_DATA_BUF) ||\
+	defined(RECORD_DP_CE_EVTS)
 
 #define HIF_CE_MAX_LATEST_HIST 2
 #define HIF_CE_MAX_LATEST_EVTS 2
@@ -207,6 +207,9 @@ struct ce_desc_hist {
 	uint8_t ce_id_hist_map[CE_COUNT_MAX];
 	bool enable[CE_COUNT_MAX];
 	bool data_enable[CE_COUNT_MAX];
+#ifdef RECORD_DP_CE_EVTS
+	uint64_t evt_mask[CE_COUNT_MAX];
+#endif
 	qdf_mutex_t ce_dbg_datamem_lock[CE_COUNT_MAX];
 	uint32_t hist_index;
 	uint32_t hist_id;
@@ -218,7 +221,7 @@ void hif_record_latest_evt(struct ce_desc_hist *ce_hist,
 			   uint8_t type,
 			   int ce_id, uint64_t time,
 			   uint32_t hp, uint32_t tp);
-#endif /*defined(HIF_CONFIG_SLUB_DEBUG_ON) || defined(HIF_CE_DEBUG_DATA_BUF)*/
+#endif /*HIF_CONFIG_SLUB_DEBUG_ON || HIF_CE_DEBUG_DATA_BUF|| RECORD_DP_CE_EVTS*/
 
 /**
  * struct hif_cfg() - store ini config parameters in hif layer
@@ -309,9 +312,12 @@ struct hif_softc {
  * Note: For MCL, #if defined (HIF_CONFIG_SLUB_DEBUG_ON) needs to be checked
  * for defined here
  */
-#if defined(HIF_CONFIG_SLUB_DEBUG_ON) || defined(HIF_CE_DEBUG_DATA_BUF)
+#if defined(HIF_CONFIG_SLUB_DEBUG_ON) || defined(HIF_CE_DEBUG_DATA_BUF) ||\
+	defined(RECORD_DP_CE_EVTS)
 	struct ce_desc_hist hif_ce_desc_hist;
-#endif /*defined(HIF_CONFIG_SLUB_DEBUG_ON) || defined(HIF_CE_DEBUG_DATA_BUF)*/
+#endif /* defined(HIF_CONFIG_SLUB_DEBUG_ON) || defined(HIF_CE_DEBUG_DATA_BUF) ||
+	* defined(RECORD_DP_CE_EVTS)
+	*/
 #ifdef IPA_OFFLOAD
 	qdf_shared_mem_t *ipa_ce_ring;
 #endif
